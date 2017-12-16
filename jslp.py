7242d493d5149ae9236b1d1c43f1fbdb66b45eb2
NiMaZi <zhengyl940425@gmail.com>
2017-12-16 18:41:06 +0100

    new baseline
    17.12.16

--- jslp.py
+++ jslp.py
@@ -1,13 +1,10 @@
 import json
 import jsonlines
 
-f=open("E:/thesiswork/test.txt","w")
-
-
-
 count=0
 with jsonlines.open("E:/thesiswork/pubmed_2017-05.jsonl") as reader:
 	for item in reader:
+		# f=open("E:/thesiswork/abs"+str(count)+".txt","w",encoding='utf-8')
 		# keylist=item.keys()
 		abstract=item["abstract"]
 		authors=item["authors"]
@@ -19,11 +16,14 @@ with jsonlines.open("E:/thesiswork/pubmed_2017-05.jsonl") as reader:
 		keywords=item["keywords"]
 		date=item["date"]
 		_id=item["_id"]
-		print(abstract)
-		f.write(abstract)
-		# print(json.dumps(item,sort_keys=True,indent=2,separators=(',',':')))
+		if str(keyterms)!="None":
+			print(keyterms)
+		if str(keywords)!="None":
+			print(keywords)
+		# f.write(abstract)
+		# f.close()
+		# print(json.dumps(units,sort_keys=False,indent=2,separators=(',',':')))
 		count=count+1
-		if count>0:
+		if count>30000:
 			break
 
-f.close()
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import urllib2
 import json
 import os
+import codecs
 from pprint import pprint
 
 REST_URL = "http://data.bioontology.org"
@@ -14,45 +15,61 @@ def get_json(url):
     return json.loads(opener.open(url).read())
 
 def print_annotations(annotations, get_class=True):
+    # annotation=(str_class id, str_class name, strlist_[synonyms list], str_ontology name, str_ontology acronym, [annotaion list])
+    # annotation list=(str_text, str_match type, int_from, int_to)
     # f=open("parse.txt","w")
+    count=0
     for result in annotations:
-        # if (len(result["annotations"])>1):
-        #     print str(result["annotations"])
+        print "processing anno "+str(count)
+        count=count+1
         class_details = get_json(result["annotatedClass"]["links"]["self"]) if get_class else result["annotatedClass"]
-        print "Class details"
-        print "\tid: " + class_details["@id"]
-        print "\tprefLabel: " + class_details["prefLabel"]
+        # print "Class details"
+        # print "\tid: " + class_details["@id"]
+        # print "\tprefLabel: " + class_details["prefLabel"]
+        # print "\tsynonyms: "
+        # for syn in class_details["synonym"]:
+        #     print syn+" "
         ontology_details = get_json(class_details["links"]["ontology"])
-        print "\tontology: " + ontology_details["name"] + "(" + ontology_details["acronym"] + ")"
+        # print "\tontology: " + ontology_details["name"] + "(" + ontology_details["acronym"] + ")"
 
-        print "Annotation details"
+        # print "Annotation details"
+        annotation_list=[]
         for annotation in result["annotations"]:
-            print "\tfrom: " + str(annotation["from"])
-            print "\tto: " + str(annotation["to"])
-            print "\tmatch type: " + annotation["matchType"]
+            # print "\ttext: " + annotation["text"]
+            # print "\tfrom: " + str(annotation["from"])
+            # print "\tto: " + str(annotation["to"])
+            # print "\tmatch type: " + annotation["matchType"]
+            annotation_list.append((annotation["text"],annotation["matchType"],annotation["from"],annotation["to"]))
 
-        if result["hierarchy"]:
-            print "\n\tHierarchy annotations"
-            for annotation in result["hierarchy"]:
-                class_details = get_json(annotation["annotatedClass"]["links"]["self"])
-                pref_label = class_details["prefLabel"] or "no label"
-                print "\t\tClass details"
-                print "\t\t\tid: " + class_details["@id"]
-                print "\t\t\tprefLabel: " + class_details["prefLabel"]
-                print "\t\t\tontology: " + class_details["links"]["ontology"]
-                print "\t\t\tdistance from originally annotated class: " + str(annotation["distance"])
+        sample_result.append((class_details["@id"],class_details["prefLabel"],class_details["synonym"],ontology_details["name"],ontology_details["acronym"],annotation_list))
+        # if result["hierarchy"]:
+        #     print "\n\tHierarchy annotations"
+        #     for annotation in result["hierarchy"]:
+        #         class_details = get_json(annotation["annotatedClass"]["links"]["self"])
+        #         pref_label = class_details["prefLabel"] or "no label"
+        #         print "\t\tClass details"
+        #         print "\t\t\tid: " + class_details["@id"]
+        #         print "\t\t\tprefLabel: " + class_details["prefLabel"]
+        #         print "\t\t\tontology: " + class_details["links"]["ontology"]
+        #         print "\t\t\tdistance from originally annotated class: " + str(annotation["distance"])
 
-        print "\n\n"
+    print "finished"
+        # print "\n\n"
 
-# text_to_annotate = "Melanoma is a malignant tumor of melanocytes which are found predominantly in skin but also in the bowel and the eye."
+# text_to_annotate = "The organ covering the body that consists of the dermis and epidermis."
+
+text_to_annotate = "Melanoma is a malignant tumor of melanocytes which are found predominantly in skin but also in the bowel and the eye."
 
 # text_to_annotate = "A term for diseases in which abnormal cells divide without control and can invade nearby tissues. Malignant cells can also spread to other parts of the body through the blood and lymph systems. There are several main types of malignancy. Carcinoma is a malignancy that begins in the skin or in tissues that line or cover internal organs. Sarcoma is a malignancy that begins in bone, cartilage, fat, muscle, blood vessels, or other connective or supportive tissue. Leukemia is a malignancy that starts in blood-forming tissue, such as the bone marrow, and causes large numbers of abnormal blood cells to be produced and enter the blood. Lymphoma and multiple myeloma are malignancies that begin in the cells of the immune system. Central nervous system cancers are malignancies that begin in the tissues of the brain and spinal cord. Also called cancer."
 
 # text_to_annotate = "American Airlines is a founding member of Oneworld alliance, the third largest airline alliance in the world and coordinates fares, services, and scheduling with alliance partners British Airways, Iberia, and Finnair in the transatlantic market and with Cathay Pacific and Japan Airlines in the transpacific market. Regional service is operated by independent and subsidiary carriers under the brand name of American Eagle.[9]"
 
-reader=open("E:/thesiswork/test.txt","r")
-text_to_annotate=reader.read()
-reader.close()
+for i in range(0,20):
+    reader=open("E:/thesiswork/abs"+str(i)+".txt","r")
+    # reader=codecs.open("E:/thesiswork/abs"+str(i)+".txt","r",encoding='utf-8')
+    # text_to_annotate=reader.read()
+    # print text_to_annotate.decode('utf-8')
+    reader.close()
 
 # Annotate using the provided text
 annotations = get_json(REST_URL + "/annotator?text=" + urllib2.quote(text_to_annotate))
@@ -64,7 +81,7 @@ annotations = get_json(REST_URL + "/annotator?text=" + urllib2.quote(text_to_ann
 
 # g=open("annotation.txt","w")
 
-annotation_tuple=[]
+# annotation_tuple=[]
 
 # count=1
 # for item in sample_result:
@@ -102,6 +119,9 @@ annotation_tuple=[]
 
 # Print out annotation details
 print_annotations(annotations)
+
+print str(len(sample_result))
+print str(sample_result)
 
 # # Annotate with hierarchy information
 # annotations = get_json(REST_URL + "/annotator?max_level=3&text=" + urllib2.quote(text_to_annotate))
