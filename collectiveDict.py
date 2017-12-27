b4f24547507e8209c4a56a66caf409ef3926e391
NiMaZi <zhengyl940425@gmail.com>
2017-12-28 00:30:25 +0100

    collD

---
+++ collectiveDict.py
@@ -0,0 +1,30 @@
+import pickle
+
+article_dict_list=[]
+entity_dict={}
+
+f=open("/home/ubuntu/results/initDict.pickle","rb")
+article_dict_list=pickle.load(f)
+f.close()
+
+count=0
+
+for entry in article_dict_list:
+	print(entry)
+	e_abs=entry['abs']
+	for key in e_abs.keys():
+		e_body=entry['body']
+		if key in entity_dict:
+			for bkey in e_body.keys():
+				if bkey in e_list:
+					entity_dict[key][bkey]=entity_dict[key][bkey]+e_body[bkey]
+				else:
+					entity_dict[key][bkey]=e_body[bkey]
+		else:
+			entity_dict[key]=e_body
+	count=count+1
+	if count>10:
+		break
+
+print(entity_dict)
+
