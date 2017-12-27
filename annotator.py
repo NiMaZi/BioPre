ff752762e49a5bc76cb2de7d72cf62d1f8b0a713
NiMaZi <zhengyl940425@gmail.com>
2017-12-27 02:11:21 +0100

    path

--- annotator.py
+++ annotator.py
@@ -1,7 +1,7 @@
-from ..ner.ner.annotator.annotator import Annotator
+from ner.ner.annotator.annotator import Annotator
 
-_annotator = Annotator("~/ner/NobleJar/NobleCoder-1.0.jar","~/ner/NobleJar/Annotator.java",searchMethod="best-match",terminology="HIVO004")
+_annotator = Annotator("ner/NobleJar/NobleCoder-1.0.jar","ner/NobleJar/Annotator.java",searchMethod="best-match",terminology="HIVO004")
 
-_filename = "~/thesiswork/data/abs0.txt"
+_filename = "thesiswork/data/abs0.txt"
 
 _annotator.process(_filename)
