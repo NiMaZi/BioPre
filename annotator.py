a9a5b31db64d8923982675eaee2681e7016f98b7
NiMaZi <zhengyl940425@gmail.com>
2017-12-27 02:18:37 +0100

    get annotations

--- annotator.py
+++ annotator.py
@@ -2,6 +2,8 @@ from ner.ner.annotator.annotator import Annotator
 
 _annotator = Annotator("ner/NobleJar/NobleCoder-1.0.jar","ner/NobleJar/Annotator.java",searchMethod="best-match",terminology="HIVO004")
 
-_filename = "thesiswork/data/abs0.txt"
-
-_annotator.process(_filename)
+for i in range(0,5):
+	_filename = "thesiswork/data/abs"+str(i)+".txt"
+	_annotator.process(_filename)
+	_filename = "thesiswork/data/body"+str(i)+".txt"
+	_annotator.process(_filename)
