cbda3718e2035be4be67d5447655937778ef9866
NiMaZi <zhengyl940425@gmail.com>
2017-12-27 23:43:23 +0100

    with body

--- initDict.py
+++ initDict.py
@@ -1,15 +1,30 @@
 import csv
 
 entity_dict={}
+entity_dict_abs={}
+entity_dict_body={}
 
 with open("/home/ubuntu/thesiswork/data/abs0.txt.mentions","r",newline='') as csvfile:
 	reader = csv.reader(csvfile)
 	for item in reader:
 		if item[2]=="ConceptName":
 			continue
-		if item[2] in entity_dict:
-			entity_dict[item[2]]=entity_dict[item[2]]+1
+		if item[2] in entity_dict_abs:
+			entity_dict_abs[item[2]]=entity_dict_abs[item[2]]+1
 		else:
-			entity_dict[item[2]]=1
+			entity_dict_abs[item[2]]=1
 
-print(sorted(entity_dict.items(),key=lambda x:x[1]))
+with open("/home/ubuntu/thesiswork/data/body0.txt.mentions","r",newline='') as csvfile:
+	reader = csv.reader(csvfile)
+	for item in reader:
+		if item[2]=="ConceptName":
+			continue
+		if item[2] in entity_dict_body:
+			entity_dict_body[item[2]]=entity_dict_body[item[2]]+1
+		else:
+			entity_dict_body[item[2]]=1
+
+entity_dict["abs"]=entity_dict_abs
+entity_dict["body"]=entity_dict_body
+
+print(entity_dict)
