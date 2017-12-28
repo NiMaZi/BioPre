3b224ee059cc4a8f8b069afddc244ffc0b33c846
NiMaZi <zhengyl940425@gmail.com>
2017-12-28 15:40:46 +0100

    tfidf
    calculate tf-idf for each entity

---
+++ tfidf.py
@@ -0,0 +1,36 @@
+import sys
+import csv
+import pickle
+
+volume=int(sys.argv[1])
+
+tf=[]
+idf=[]
+word_list=set([])
+
+for i in range(0,volume):
+	tmp_tf={}
+	with open("/home/ubuntu/thesiswork/data/abs"+str(i)+".txt.mentions","r",newline='',encoding='utf-8') as csvfile:
+		reader=csv.reader(csvfile)
+		for item in reader:
+			if item[2]=="ConceptName":
+				continue
+			word_list.add(item[2])
+			if item[2] in tmp_tf:
+				tmp_tf[item[2]]+=1
+			else:
+				tmp_tf[item[2]]=1
+	with open("/home/ubuntu/thesiswork/data/body"+str(i)+".txt.mentions","r",newline='',encoding='utf-8') as csvfile:
+		reader=csv.reader(csvfile)
+		for item in reader:
+			if item[2]=="ConceptName":
+				continue
+			word_list.add(item[2])
+			if item[2] in tmp_tf:
+				tmp_tf[item[2]]+=1
+			else:
+				tmp_tf[item[2]]=1
+	tf.append(tmp_tf)
+
+print(tf)
+print(word_list)
