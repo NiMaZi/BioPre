1871564afb5e25a6f558252743ee91a07c4ae40b
NiMaZi <zhengyl940425@gmail.com>
2017-12-28 02:13:42 +0100

    update

--- collectiveDict.py
+++ collectiveDict.py
@@ -23,14 +23,14 @@ for entry in article_dict_list:
 	for key in e_abs.keys():
 		n_body={k:float(e_body[k]/e_abs[key]) for k in e_body.keys()}
 		if key in entity_dict:
-			entity_dict[key][0]=entity_dict[key][0]+e_abs[key]
+			entity_dict[key][0]=entity_dict[key][0]+1.0
 			oDict=entity_dict[key][1]
-			_tmp={k : oDict.get(k, 0) + n_body.get(k,0) for k in set(oDict.keys()) | set(n_body.keys())}
+			_tmp={k:oDict[k]+n_body[k] for k in set(oDict.keys())|set(n_body.keys())}
 			entity_dict[key][1]=_tmp
 		else:
-			entity_dict[key]=[e_abs[key],n_body]
+			entity_dict[key]=[1.0,n_body]
 	count=count+1
-	if count>2:
+	if count>5:
 		break
 
 for key in entity_dict.keys():
