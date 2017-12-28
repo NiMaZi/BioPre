508ca57fb7590c84512fcd756c275ca3689387e1
NiMaZi <zhengyl940425@gmail.com>
2017-12-28 02:09:34 +0100

    normalize

--- collectiveDict.py
+++ collectiveDict.py
@@ -10,50 +10,25 @@ f.close()
 count=0
 
 for entry in article_dict_list:
-	# print("abs:\n")
-	# print(entry['abs'])
-	# print("\n")
-	# print("body:\n")
-	# print(entry['body'])
-	# print("\n\n")
+	print("abs:\n")
+	print(entry['abs'])
+	print("\n")
+	print("body:\n")
+	print(entry['body'])
+	print("\n\n")
 	e_abs=entry['abs']
 	e_body=entry['body']
+	if not e_abs:
+		continue
 	for key in e_abs.keys():
-		# print("entity name in abs:")
-		# print(key)
+		n_body={k:float(e_body[k]/e_abs[key]) for k in e_body.keys()}
 		if key in entity_dict:
-			# print("hit abs mention")
 			entity_dict[key][0]=entity_dict[key][0]+e_abs[key]
 			oDict=entity_dict[key][1]
-			_tmp={k : oDict.get(k, 0) + e_body.get(k,0) for k in set(oDict.keys()) | set(e_body.keys())}
+			_tmp={k : oDict.get(k, 0) + n_body.get(k,0) for k in set(oDict.keys()) | set(n_body.keys())}
 			entity_dict[key][1]=_tmp
-			# for bkey in e_body.keys():
-			# 	# print("\tentity name in body:")
-			# 	# print(bkey)
-			# 	if bkey in entity_dict[key][1]:
-			# 		# print("\thit body mention")
-			# 		# print(key)
-			# 		# print(entity_dict[key][1])
-			# 		entity_dict[key][1][bkey]=entity_dict[key][1][bkey]+e_body[bkey]
-			# 		# print(key)
-			# 		# print(entity_dict[key][1])
-			# 		# print(entity_dict["Protein"])
-			# 	else:
-			# 		# print("\tadd body mention")
-			# 		# print(key)
-			# 		# print(entity_dict[key][1])
-			# 		entity_dict[key][1][bkey]=e_body[bkey]
-			# 		# print(key)
-			# 		# print(entity_dict[key][1])
-			# 		# print(entity_dict["Protein"])
 		else:
-			# print("add abs mention")
-			entity_dict[key]=[e_abs[key],e_body]
-	# print("\n\n")
-	# for key in entity_dict.keys():
-	# 	print(key)
-	# 	print(entity_dict[key])
-	# 	print("\n")
+			entity_dict[key]=[e_abs[key],n_body]
 	count=count+1
 	if count>2:
 		break
