d7d3c963b6279b8944514efcdcb1b06ee4b77d89
NiMaZi <zhengyl940425@gmail.com>
2018-01-08 13:11:01 +0100

    new proc

--- jslp.py
+++ jslp.py
@@ -4,10 +4,10 @@ import jsonlines
 count=0
 keyterms_count=0
 keywords_count=0
-with jsonlines.open("../pubmed_2017-05.jsonl") as reader:
+with jsonlines.open("/home/ubuntu/pubmed_2017-05.jsonl") as reader:
 	for item in reader:
-		f=open("../thesiswork/data/body"+str(count)+".txt","w",encoding='utf-8')
-		g=open("../thesiswork/data/abs"+str(count)+".txt","w",encoding='utf-8')
+		# f=open("/home/ubuntu/thesiswork/data/body"+str(count)+".txt","w",encoding='utf-8')
+		# g=open("/home/ubuntu/thesiswork/data/abs"+str(count)+".txt","w",encoding='utf-8')
 		# keylist=item.keys()
 		abstract=item["abstract"]
 		authors=item["authors"]
@@ -25,17 +25,20 @@ with jsonlines.open("../pubmed_2017-05.jsonl") as reader:
 		# if str(keywords)!="None":
 		# 	keywords_count=keywords_count+1
 			# print(keywords)
-		# print(body)
-		f.write(str(body))
-		f.close()
-		g.write(abstract)
-		g.close()
+		print("hey\n")
+		print(keywords)
+		print("hey\n")
+		print(title)
+		# f.write(str(body))
+		# f.close()
+		# g.write(abstract)
+		# g.close()
 		# if count==177:
 		# 	print(json.dumps(item,sort_keys=False,indent=2,separators=(',',':')))
 		# 	# print(str(body))
 		# 	break
 		count=count+1
-		# if count>500:
-		# 	break
+		if count>5:
+			break
 	print(count)
 	# print(count,keywords_count,keyterms_count)
