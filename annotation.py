49b4deb2116ef27d54ee480723603109a5ba7d6c
NiMaZi <zhengyl940425@gmail.com>
2018-01-14 21:59:11 +0100

    rollback

--- annotation.py
+++ annotation.py
@@ -1,17 +1,17 @@
 from time import time
 from ner.ner.annotator.annotator import Annotator
 
-_annotator = Annotator("ner/NobleJar/NobleCoder-1.0.jar","ner/NobleJar/Annotator.java",searchMethod="best-match",terminology="HIVO004")
+_annotator = Annotator("ner/NobleJar/NobleCoder-1.0.jar","ner/NobleJar/Annotator.java",searchMethod="best-match",terminology="GAMUTS")
 
 start=time()
-for i in range(0,21772):
-	_filename = "thesiswork/data2/abs"+str(i)+".txt"
+for i in range(0,12935):
+	_filename = "thesiswork/kdata/abs"+str(i)+".txt"
 	_annotator.process(_filename)
-	_filename = "thesiswork/data2/body"+str(i)+".txt"
+	_filename = "thesiswork/kdata/body"+str(i)+".txt"
 	_annotator.process(_filename)
-	_filename = "thesiswork/data2/title"+str(i)+".txt"
+	_filename = "thesiswork/kdata/title"+str(i)+".txt"
 	_annotator.process(_filename)
-	_filename = "thesiswork/data2/keywords"+str(i)+".txt"
+	_filename = "thesiswork/kdata/keywords"+str(i)+".txt"
 	_annotator.process(_filename)
 end=time()
 
