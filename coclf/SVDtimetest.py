13e82edd887cf10eff2a65aca7f44eace1baa86d
NiMaZi <zhengyl940425@gmail.com>
2018-01-15 11:47:16 +0100

    rbf only

--- coclf/SVDtimetest.py
+++ coclf/SVDtimetest.py
@@ -6,6 +6,7 @@ from time import time
 from sklearn import svm
 
 volume=int(sys.argv[1])
+g_ratio=float(sys.argv[2])
 
 f=open("/home/ubuntu/results/coclf/svd_trainlist.pkl","rb")
 training_list=pickle.load(f)
@@ -15,18 +16,18 @@ sample=random.sample(training_list,volume)
 
 # print(sample)
 
-clf_linear=svm.OneClassSVM(kernel='linear')
-clf_rbf=svm.OneClassSVM(kernel='rbf',gamma=(1.0/38.0)*10)
+# clf_linear=svm.OneClassSVM(kernel='linear')
+clf_rbf=svm.OneClassSVM(kernel='rbf',gamma=(1.0/38.0)*g_ratio)
 
 n_training=np.array(sample)
 
-start=time()
+# start=time()
 
-clf_linear.fit(n_training)
+# clf_linear.fit(n_training)
 
-end=time()
+# end=time()
 
-print(end-start)
+# print(end-start)
 
 start=time()
 
@@ -36,9 +37,9 @@ end=time()
 
 print(end-start)
 
-f=open("/home/ubuntu/results/coclf/svd_linear.pkl","wb")
-pickle.dump(clf_linear,f)
-f.close()
+# f=open("/home/ubuntu/results/coclf/svd_linear.pkl","wb")
+# pickle.dump(clf_linear,f)
+# f.close()
 
 f=open("/home/ubuntu/results/coclf/svd_rbf_default.pkl","wb")
 pickle.dump(clf_rbf,f)
