b1e8018328c5252a75b757a39eb643ed84ae181b
NiMaZi <zhengyl940425@gmail.com>
2018-01-17 15:13:47 +0100

    f

--- coclf/bSVDbalancedTrain.py
+++ coclf/bSVDbalancedTrain.py
@@ -9,13 +9,14 @@ pos_list=[]
 neg_prelist=[]
 
 for chunk in range(0,34):
-	with open("/home/ubuntu/results/coclf/bsvd_trainlist"+str(chunk)+".pkl","rb") as f:
-		_list=pickle.load(f)
-		for item in _list:
-			if item[38]==1:
-				pos_list.append(item)
-			else:
-				neg_prelist.append(item)
+	f=open("/home/ubuntu/results/coclf/bsvd_trainlist"+str(chunk)+".pkl","rb")
+	_list=pickle.load(f)
+	f.close()
+	for item in _list:
+		if item[38]==1:
+			pos_list.append(item)
+		else:
+			neg_prelist.append(item)
 
 neg_list=random.sample(neg_prelist,len(pos_list))
 
