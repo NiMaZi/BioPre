9dccc342561c1ea8b4b6a9ce0ea62d2525db43e7
NiMaZi <zhengyl940425@gmail.com>
2018-01-17 16:03:16 +0100

    blvd train

--- coclf/bSVDbalancedTrain.py
+++ coclf/bSVDbalancedTrain.py
@@ -24,4 +24,20 @@ pos_list.extend(neg_list)
 train_list=list(pos_list)
 random.shuffle(train_list)
 
-print(len(train_list))
+n_train=np.array(train_list)
+n_train_X=n_train[:,0:38]
+n_train_y=n_train[:,38]
+
+clf_linear=svm.LinearSVC()
+clf_rbf=svm.SVC(gamma=1.0/38.0)
+
+clf_linear.fit(n_train_X,n_train_y)
+clf_rbf.fit(n_train_X,n_train_y)
+
+f=open("/home/ubuntu/results/coclf/bsvd_linear.pkl","wb")
+pickle.dump(clf_linear,f)
+f.close()
+
+f=open("/home/ubuntu/results/coclf/bsvd_rbf_default.pkl","wb")
+pickle.dump(clf_rbf,f)
+f.close()
