45c92415978fdd1f608f9b4f113283b8bd1684ff
NiMaZi <zhengyl940425@gmail.com>
2018-01-24 14:03:39 +0100

    test

--- saliency/bSVMsample.py
+++ saliency/bSVMsample.py
@@ -39,7 +39,7 @@ p_count=0
 b_count=0
 u_count=0
 
-for i in range(0,len(dis_list)):
+for i in range(0,20):
 	label_set=set()
 	body_set=set()
 	for item in dis_list[i]['title']:
@@ -49,6 +49,9 @@ for i in range(0,len(dis_list)):
 	for item in dis_list[i]['body']:
 		body_set.add(item[1])
 	union_set=(label_set|body_set)
+	print(label_set)
+	print(keywords_list[i])
+	print(featured_list[i]['abs'].keys())
 	for key in featured_list[i]['abs'].keys():
 		count+=1
 		if key in label_set or key.lower() in keywords_list[i]:
