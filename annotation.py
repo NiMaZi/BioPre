a64440ae69d89cc21eaf36b7aae68171a49095fb
NiMaZi <zhengyl940425@gmail.com>
2018-01-25 16:14:22 +0100

    test it!

--- annotation.py
+++ annotation.py
@@ -1,19 +1,64 @@
-from time import time
+import os
 from ner.ner.annotator.annotator import Annotator
 
-_annotator = Annotator("ner/NobleJar/NobleCoder-1.0.jar","ner/NobleJar/Annotator.java",searchMethod="best-match",terminology="GAMUTS")
+term_dir="/home/ubuntu/.noble/terminologies/"
 
-start=time()
-for i in range(0,12935):
-	_filename = "thesiswork/kdata/abs"+str(i)+".txt"
-	_annotator.process(_filename)
-	_filename = "thesiswork/kdata/body"+str(i)+".txt"
-	_annotator.process(_filename)
-	_filename = "thesiswork/kdata/title"+str(i)+".txt"
-	_annotator.process(_filename)
-	_filename = "thesiswork/kdata/keywords"+str(i)+".txt"
-	_annotator.process(_filename)
-end=time()
+term_list=[]
+for file in os.listdir(file_dir):
+	if file.split(".")[0]:
+		term_list.append(file.split(".")[0])
 
-with open("log.txt","w") as log:
-	log.write("time used: "+str(end-start)+" seconds")
+# _annotator = Annotator("ner/NobleJar/NobleCoder-1.0.jar","ner/NobleJar/Annotator.java",searchMethod="best-match",terminology="GAMUTS")
+
+annotator_list=[]
+for term in term_list:
+	_annotator = Annotator("ner/NobleJar/NobleCoder-1.0.jar","ner/NobleJar/Annotator.java",searchMethod="best-match",terminology=term)
+	annotator_list.append(_annotator)
+
+
+for i in range(0,2000): # max 12935
+	cf_abs=open("/home/ubuntu/thesiswork/kdata/abs"+str(i)+".csv","a")
+	cf_body=open("/home/ubuntu/thesiswork/kdata/body"+str(i)+".csv","a")
+	cf_title=open("/home/ubuntu/thesiswork/kdata/title"+str(i)+".csv","a")
+	cf_keywords=open("/home/ubuntu/thesiswork/kdata/keywords"+str(i)+".csv","a")
+	for j in range(0,len(term_list)):
+		# _annotator = Annotator("ner/NobleJar/NobleCoder-1.0.jar","ner/NobleJar/Annotator.java",searchMethod="best-match",terminology=term)
+		
+		_filename = "thesiswork/kdata/abs"+str(i)+".txt"
+		annotator_list[j].process(_filename)
+
+		f=open("/home/ubuntu/thesiswork/kdata/abs"+str(i)+".txt.mentions")
+		_str=f.read()
+		cf_abs.write(_str)
+		f.close()
+
+		_filename = "thesiswork/kdata/body"+str(i)+".txt"
+		annotator_list[j].process(_filename)
+
+		f=open("/home/ubuntu/thesiswork/kdata/body"+str(i)+".txt.mentions")
+		_str=f.read()
+		cf_body.write(_str)
+		f.close()
+
+		_filename = "thesiswork/kdata/title"+str(i)+".txt"
+		annotator_list[j].process(_filename)
+
+		f=open("/home/ubuntu/thesiswork/kdata/title"+str(i)+".txt.mentions")
+		_str=f.read()
+		cf_title.write(_str)
+		f.close()
+
+		_filename = "thesiswork/kdata/keywords"+str(i)+".txt"
+		annotator_list[j].process(_filename)
+
+		f=open("/home/ubuntu/thesiswork/kdata/keywords"+str(i)+".txt.mentions")
+		_str=f.read()
+		cf_keywords.write(_str)
+		f.close()
+
+	cf_abs.close()
+	cf_body.close()
+	cf_title.close()
+	cf_keywords.close()
+
+
