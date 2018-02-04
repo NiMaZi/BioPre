79f6eb04d5cb20e02c73eddcbbcc1ec57d5d5a53
NiMaZi <zhengyl940425@gmail.com>
2018-02-04 16:00:09 +0100

    set options

--- demo.py
+++ demo.py
@@ -21,12 +21,12 @@ body_dict=featured_list[article_id]['body']
 print("entities mentioned in the abstract:\n")
 
 for key in abs_dict.keys():
-	print(key+" ")
+	print(key)
 
 print("\nentities mentioned in the body:\n")
 
 for key in body_dict.keys():
-	print(key+" ")
+	print(key)
 
 print("\nentities predicted:\n")
 
@@ -98,14 +98,17 @@ for a_key in abs_dict.keys():
 			dev_cor=0.0
 		sample_input=np.array([[a_centrality,b_centrality,dev_cor,pred_saliency]])
 		pred_label_rbf=list(clf_sgd.predict(sample_input))[0]
-		# if pred_label_rbf==1:
-		# 	pred_label_correction=list(clf_sgd_correction.predict(sample_input))[0]
-		# 	pred_label_filter=list(clf_sgd_filter.predict(sample_input))[0]
-		# 	pred_label_rbf=cor_rate*pred_label_correction+(1.0-cor_rate)*pred_label_filter
-		# 	if pred_label_rbf>0.5:
-		# 		pred_label_rbf=1
-		# 	else:
-		# 		pred_label_rbf=0
+		if pred_label_rbf==1:
+			pred_label_correction=list(clf_sgd_correction.predict(sample_input))[0]
+			pred_label_filter=list(clf_sgd_filter.predict(sample_input))[0]
+			if cor_rate==-1:
+				pred_label_rbf=1
+			else:
+				pred_label_rbf=cor_rate*pred_label_correction+(1.0-cor_rate)*pred_label_filter
+				if pred_label_rbf>0.5:
+					pred_label_rbf=1
+				else:
+					pred_label_rbf=0
 		if pred_label_rbf==1:
 			if b_key in pred_dict_rbf.keys():
 				pred_dict_rbf[b_key]+=1.0
@@ -123,4 +126,4 @@ for key in pred_dict_rbf.keys():
 		pred_set_rbf.add(key)
 
 for entity in pred_set_rbf:
-	print(entity+" ")
+	print(entity)
