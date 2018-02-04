453234a09a4ee202dc7685b95a8a0049286d3d23
NiMaZi <zhengyl940425@gmail.com>
2018-02-04 15:46:22 +0100

    predict

--- demo.py
+++ demo.py
@@ -69,3 +69,47 @@ f.close()
 f=open("/home/ubuntu/results/coclf/clf_sgd_filter.pkl","rb")
 clf_sgd_filter=pickle.load(f)
 f.close()
+
+sample_input=[]
+pred_dict_rbf={}
+max_conf_rbf=0
+for a_key in abs_dict.keys():
+	pred_input=np.array([[key_phrase[word_list.index(a_key)],abs_dict[a_key][0],abs_dict[a_key][1]-abs_dict[a_key][0],abs_dict[a_key][2],idf[word_list.index(a_key)],centrality[a_key]]])
+	pred_saliency=list(s_clf.predict(pred_input))[0]
+	for b_key in word_list:
+		if a_key==b_key:
+			continue
+		label=0
+		if b_key in body_dict.keys():
+			label=1
+		else:
+			label=0
+		print(count,a_key,b_key,label)
+		sample_input=np.array([[centrality[a_key],centrality[b_key],dev_mat[word_list.index(a_key)][word_list.index(b_key)],pred_saliency]])
+		pred_label_rbf=list(clf_sgd.predict(sample_input))[0]
+		if pred_label_rbf==1:
+			pred_label_correction=list(clf_sgd_correction.predict(sample_input))[0]
+			pred_label_filter=list(clf_sgd_filter.predict(sample_input))[0]
+			pred_label_rbf=cor_rate*pred_label_correction+(1.0-cor_rate)*pred_label_filter
+			if pred_label_rbf>0.5:
+				pred_label_rbf=1
+			else:
+				pred_label_rbf=0
+		if pred_label_rbf==1:
+			if b_key in pred_dict_rbf.keys():
+				pred_dict_rbf[b_key]+=1.0
+				if pred_dict_rbf[b_key]>max_conf_rbf:
+					max_conf_rbf=pred_dict_rbf[b_key]
+			else:
+				pred_dict_rbf[b_key]=1.0
+				if pred_dict_rbf[b_key]>max_conf_rbf:
+					max_conf_rbf=pred_dict_rbf[b_key]
+for key in pred_dict_rbf.keys():
+	pred_dict_rbf[key]/=max_conf_rbf
+pred_set_rbf=set()
+for key in pred_dict_rbf.keys():
+	if pred_dict_rbf[key]>confidence:
+		pred_set_rbf.add(key)
+
+for entity in pred_set_rbf:
+	print(entity+" ")
