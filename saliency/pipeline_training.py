e9d770a8fb8d4c3b93d01d4e055db812e22ce363
NiMaZi <zhengyl940425@gmail.com>
2018-02-06 16:33:29 +0100

    a pipeline

---
+++ saliency/pipeline_training.py
@@ -0,0 +1,14 @@
+import csv
+import sys
+import math
+import random
+import numpy as np
+from sklearn import svm
+from difflib import SequenceMatcher as Sqm
+
+def test(s,d):
+	print(s,d)
+
+if __name__=="__main__":
+	d=int(sys.argv[1])
+	test("hey",d)
