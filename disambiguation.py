a94b445d5b2e6ab15f6b53e609c3ede68de69769
NiMaZi <zhengyl940425@gmail.com>
2018-02-06 01:27:53 +0100

    discard

--- disambiguation.py
+++ disambiguation.py
@@ -7,28 +7,24 @@ import subprocess
 
 # Document	Matched Term	Code	Concept Name	Semantic Type	Annotations	Certainty	ContextualAspect	ContextualModality	Degree	Experiencer	Permanence	Polarity	Temporality
 
-for i in range(1,2):
-	subprocess.call(['java','-jar','/home/ubuntu/ner/NobleJar/NobleCoder-1.0.jar','-terminology','NCI_Thesaurus','-input','/home/ubuntu/thesiswork/kdata/abs'+str(i)+'.txt','-output','/home/ubuntu/thesiswork/kdata/disambiguation','-search','best-match','-selectBestCandidates'])
-	f=open('/home/ubuntu/thesiswork/kdata/disambiguation/RESULTS.tsv','r',encoding='utf-8')
-	unamb=f.read()
-	f.close()
-	tmp_list=unamb.split('\n')
-	unamb_list=[]
-	for item in tmp_list:
-		unamb_list.append(item.split('\t'))
-	f=open('/home/ubuntu/thesiswork/kdata/abs'+str(i)+'.txt.mentions','r',encoding='utf-8')
-	rd=csv.reader(f)
-	j=1
-	result_list=[]
-	result_list.append(['Mention','ConceptCode','ConceptName','Synonyms','SemanticType','Start','End'])
-	for item in rd:
-		print(item[1],unamb_list[j][2])
-		if item[1]==unamb_list[j][2]:
-			result_list.append([item[0],item[1],item[2],item[3],unamb_list[j][4],item[4],item[5]])
-			j+=1
-	f.close()
-	f=open('/home/ubuntu/thesiswork/kdata/abs'+str(i)+'.csv','w',encoding='utf-8')
-	wr=csv.writer(f)
-	for row in result_list:
-		wr.writerow(row)
-	f.close()
+fname=['abs','body','title','keywords']
+
+for i in range(0,12935):
+	for j in range(0,4):
+		subprocess.call(['java','-jar','/home/ubuntu/ner/NobleJar/NobleCoder-1.0.jar','-terminology','NCI_Thesaurus','-input','/home/ubuntu/thesiswork/kdata/'+fname[j]+str(i)+'.txt','-output','/home/ubuntu/thesiswork/kdata/disambiguation','-search','best-match','-selectBestCandidates'])
+		f=open('/home/ubuntu/thesiswork/kdata/disambiguation/RESULTS.tsv','r',encoding='utf-8')
+		unamb=f.read()
+		f.close()
+		tmp_list=unamb.split('\n')
+		unamb_list=[]
+		for item in tmp_list:
+			unamb_list.append(item.split('\t'))
+		result_list=[]
+		result_list.append(['Mention','ConceptCode','ConceptName','SemanticType','Start'])
+		for item in unamb_list:
+			result_list.append([item[1],item[2],item[3],item[4],item[5].split('/')[1]])
+		f=open('/home/ubuntu/thesiswork/kdata/'+fname[j]+str(i)+'.csv','w',encoding='utf-8')
+		wr=csv.writer(f)
+		for row in result_list:
+			wr.writerow(row)
+		f.close()
