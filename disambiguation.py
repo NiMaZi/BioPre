b1805c93c4117f94ccd667899e3c5d1b7c8f1388
NiMaZi <zhengyl940425@gmail.com>
2018-02-06 01:41:02 +0100

    f

--- disambiguation.py
+++ disambiguation.py
@@ -9,7 +9,7 @@ import subprocess
 
 fname=['abs','body','title','keywords']
 
-for i in range(0,12935):
+for i in range(0,1):
 	for j in range(0,4):
 		subprocess.call(['java','-jar','/home/ubuntu/ner/NobleJar/NobleCoder-1.0.jar','-terminology','NCI_Thesaurus','-input','/home/ubuntu/thesiswork/kdata/'+fname[j]+str(i)+'.txt','-output','/home/ubuntu/thesiswork/kdata/disambiguation','-search','best-match','-selectBestCandidates'])
 		f=open('/home/ubuntu/thesiswork/kdata/disambiguation/RESULTS.tsv','r',encoding='utf-8')
@@ -21,11 +21,9 @@ for i in range(0,12935):
 			unamb_list.append(item.split('\t'))
 		result_list=[]
 		result_list.append(['Mention','ConceptCode','ConceptName','SemanticType','Start'])
-		unamb_list.pop(0)
+		unamb_list=unamb_list[1:len(unamb_list)-1]
 		for item in unamb_list:
-			print(item[5])
-			print(item[5].split('/'))
-			result_list.append([item[1],item[2],item[3],item[4],item[5].split('/')[1]])
+			result_list.append([item[1],item[2],item[3],item[4],item[5].split(',')[0].split('/')[1]])
 		f=open('/home/ubuntu/thesiswork/kdata/'+fname[j]+str(i)+'.csv','w',encoding='utf-8')
 		wr=csv.writer(f)
 		for row in result_list:
