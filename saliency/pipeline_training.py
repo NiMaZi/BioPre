a851e610738ce593bd75d9a8b07f713c9cf18281
NiMaZi <zhengyl940425@gmail.com>
2018-02-07 11:20:29 +0100

    try import

--- saliency/pipeline_training.py
+++ saliency/pipeline_training.py
@@ -1,19 +1,23 @@
 import csv
 import sys
+import json
 import math
 import random
 import numpy as np
 from sklearn import svm
 from difflib import SequenceMatcher as Sqm
 
+from .semantic_type import isolated_num
+from .semantic_type import isolated_list
+
 volume=int(sys.argv[1])
 samples=[]
 
-for i in range(0,500):
+for i in range(0,volume):
 	f=open("/home/ubuntu/thesiswork/kdata/abs"+str(i)+".txt",'r')
 	doc=f.read()
 	abs_length=len(doc)
 	f.close()
 	abs_dict={}
+	print(isolated_num)
 	f=open("/home/ubuntu/thesiswork/kdata/abs"+str(i)+".csv",'r',encoding='utf-8')
-	
