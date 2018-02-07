f939307625d7a553fd41a9be2d6c0277ba9e8663
NiMaZi <zhengyl940425@gmail.com>
2018-02-07 12:08:21 +0100

    print

--- saliency/pipeline_training.py
+++ saliency/pipeline_training.py
@@ -1,7 +1,8 @@
-# import csv
+import csv
 import sys
-# import json
-# import math
+import json
+import math
+import pickle
 # import random
 # import numpy as np
 # from sklearn import svm
@@ -11,13 +12,76 @@ from semantic_type import isolated_num
 from semantic_type import isolated_list
 
 volume=int(sys.argv[1])
+secondary_match_rate=float(sys.argv[1])
 samples=[]
+key_phrase={}
+
+f=open("/home/ubuntu/results/saliency/keywords_list","rb")
+keywords_list=pickle.load(f)
+f.close()
+
+f=open("/home/ubuntu/results_new/ontology/word_list.json","r")
+word_list=json.load(f)
+f.close()
+
+for w in word_list:
+	key_phrase[w]=0.0
 
 for i in range(0,volume):
+
 	f=open("/home/ubuntu/thesiswork/kdata/abs"+str(i)+".txt",'r')
 	doc=f.read()
 	abs_length=len(doc)
 	f.close()
+
+	salient_mentions=set()
+
+	f=open("/home/ubuntu/thesiswork/kdata/title"+str(i)+".csv",'r',encoding='utf-8')
+	reader=csv.reader(f)
+	for item in reader:
+		if item[1]=='ConceptCode':
+			continue
+		salient_mentions.add(item[1])
+	f.close()
+
+	f=open("/home/ubuntu/thesiswork/kdata/keywords"+str(i)+".csv",'r',encoding='utf-8')
+	reader=csv.reader(f)
+	for item in reader:
+		if item[1]=='ConceptCode':
+			continue
+		salient_mentions.add(item[1])
+	f.close()
+
 	abs_dict={}
-	print(isolated_num)
+
 	f=open("/home/ubuntu/thesiswork/kdata/abs"+str(i)+".csv",'r',encoding='utf-8')
+	reader=csv.reader(f)
+	for item in reader:
+		if item[1]=='ConceptCode':
+			continue
+		if item[1] in abs_dict.keys():
+			abs_dict[item[1]][0]+=1.0
+			new_dis=float(item[4])/float(abs_length)
+			if new_dis>abs_dict[item[1]][2]:
+				abs_dict[item[1]][2]=new_dis
+		else:
+			s_type=isolated_list.index(item[3].split('|')[0])
+			dis=float(item[4])/float(abs_length)
+			label=0
+			if item[1] in salient_mentions:
+				label=1
+			else:
+				for kw in keywords_list[i]:
+					if Sqm(None,kw,item[0].lower()).ratio()>=secondary_match_rate:
+						label=1
+						break
+			abs_dict[item[1]]=[1.0,dis,dis,s_type,label]
+
+	for key in abs_dict.keys():
+		samples.append([key,abs_dict[key][0],abs_dict[key][1],abs_dict[key][2]-abs_dict[key][1],abs_dict[key][3],abs_dict[key][4]])
+		key_phrase[key]+=abs_dict[key][4]
+
+for sample in samples:
+	sample=[key_phrase[sample[0]]]+sample[1:]
+
+print(samples)
