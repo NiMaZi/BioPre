51eb3134cb3674b47e4a65a7bca1b03dece6e222
NiMaZi <zhengyl940425@gmail.com>
2018-02-08 17:37:36 +0100

    train another sgd

--- coclf/SVMtrainBsample.py
+++ coclf/SVMtrainBsample.py
@@ -3,7 +3,6 @@ import math
 import pickle
 import random
 import numpy as np
-# from sklearn import svm
 from sklearn import linear_model as lm
 
 def tree_distance(vec_1,vec_2):
@@ -97,21 +96,7 @@ for i in range(0,int(len(featured_list)*split_ratio)):
 					pos_list.append(_list)
 				else:
 					label=0
-					# _list=[dev_mat[word_list.index(a_key_1)][word_list.index(b_key)],dev_mat[word_list.index(a_key_2)][word_list.index(b_key)]]
-					# _list.extend(word2tvec[a_key_1])
-					# _list.extend(word2tvec[a_key_2])
-					# _list.extend(word2tvec[b_key])
 					_list.append(label)
-					# _list.extend([a_key_1,a_key_2,b_key])
-					# pos_list.append(_list)
-					# pos_list.append([centrality[a_key_1],centrality[a_key_2],centrality[b_key],dev_mat[word_list.index(a_key_1)][word_list.index(b_key)],dev_mat[word_list.index(a_key_1)][word_list.index(b_key)],pred_saliency_1,pred_saliency_2,label])
-				# else:
-				# 	label=0
-					# _list=[dev_mat[word_list.index(a_key_1)][word_list.index(b_key)],dev_mat[word_list.index(a_key_2)][word_list.index(b_key)]]
-					# _list.extend(word2tvec[a_key_1])
-					# _list.extend(word2tvec[a_key_2])
-					# _list.append(label)
-					# _list.extend([a_key_1,a_key_2,b_key])
 					neg_prelist.append(_list)
 					# neg_prelist.append([centrality[a_key_1],centrality[a_key_2],centrality[b_key],dev_mat[word_list.index(a_key_1)][word_list.index(b_key)],dev_mat[word_list.index(a_key_1)][word_list.index(b_key)],pred_saliency_1,pred_saliency_2,label])
 				# print(count,a_key_1,a_key_2,b_key,label)
@@ -121,43 +106,17 @@ for i in range(0,int(len(featured_list)*split_ratio)):
 		# sample_prelist=pos_list+neg_prelist
 		sample_prelist=random.sample(neg_prelist,len(pos_list))
 		sample_prelist.extend(pos_list)
-		# random.shuffle(sample_prelist)
-		# outlog=random.sample(sample_prelist,100)
 		tlen=len(sample_prelist[0])
-
-# print(len(sample_prelist))
-
-# clf_linear=svm.LinearSVC()
-# clf_rbf=svm.SVC(gamma=0.25)
-
-# clf_lr=lm.LogisticRegression()
-
-
 		nTrain=np.array(sample_prelist)
 		nX=nTrain[:,0:tlen-1]
 		ny=nTrain[:,tlen-1]
-
-# clf_linear.fit(nX,ny)
-# clf_rbf.fit(nX,ny)
-# clf_lr.fit(nX,ny)
 		clf_sgd.partial_fit(nX,ny,classes=[0,1])
 		pos_list=[]
 		neg_prelist=[]
 		count=0
 
-# f=open("/home/ubuntu/results/coclf/clf_linear.pkl","wb")
-# pickle.dump(clf_linear,f)
-# f.close()
-
-# f=open("/home/ubuntu/results/coclf/clf_rbf.pkl","wb")
-# pickle.dump(clf_rbf,f)
-# f.close()
-
-# f=open("/home/ubuntu/results/coclf/b_clf_lr.pkl","wb")
-# pickle.dump(clf_lr,f)
-# f.close()
-
+f=open("/home/ubuntu/results/coclf/b_clf_sgd_plusdis.pkl","wb")
+pickle.dump(clf_sgd,f)
+f.close()
+print(sys.argv[0])
 print(t_count)
-# f=open("/home/ubuntu/results/coclf/b_clf_sgd_test_dis.pkl","wb")
-# pickle.dump(clf_sgd,f)
-# f.close()
