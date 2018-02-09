62719319ea6deb1f0a4b108f4e7840c18de945db
NiMaZi <zhengyl940425@gmail.com>
2018-02-10 00:30:52 +0100

    new plot

--- coclf/SVMtestBsample.py
+++ coclf/SVMtestBsample.py
@@ -1,14 +1,26 @@
 import sys
 import math
-import random
 import pickle
 import numpy as np
-from sklearn import svm
 from sklearn import linear_model as lm
 
 front_split_ratio=float(sys.argv[1])
 end_split_ratio=float(sys.argv[2])
-confidence=float(sys.argv[3])
+# confidence=float(sys.argv[3])
+
+def tree_distance(vec_1,vec_2):
+	distance=0.0
+	for i in range(0,len(vec_1)):
+		if not vec_1[i]==vec_2[i]:
+			for j in range(i,len(vec_1)):
+				if vec_1[j]==-1.0:
+					break
+				distance+=1.0
+			for j in range(i,len(vec_2)):
+				if vec_2[j]==-1.0:
+					break
+				distance+=1.0
+	return distance
 
 f=open("/home/ubuntu/results/saliency/featured.pkl","rb")
 featured_list=pickle.load(f)
@@ -42,122 +54,77 @@ f=open("/home/ubuntu/results/saliency/simplemat.pkl","rb")
 dev_mat=pickle.load(f)
 f.close()
 
-f=open("/home/ubuntu/results/coclf/b_clf_lr.pkl","rb")
-clf_lr=pickle.load(f)
-f.close()
-
-f=open("/home/ubuntu/results/coclf/b_clf_sgd_test.pkl","rb")
+f=open("/home/ubuntu/results/coclf/b_clf_sgd_plusdis_minusvec.pkl","rb")
 clf_sgd=pickle.load(f)
 f.close()
 
-count=0
-
-# tp_linear=0.0
-# fp_linear=0.0
-# fn_linear=0.0
-tp_rbf=0.0
-fp_rbf=0.0
-fn_rbf=0.0
-log_list=[]
-
-# for i in range(int(front_split_ratio*len(featured_list)),int(end_split_ratio*len(featured_list))):
-for i in range(int(front_split_ratio*len(featured_list)),int(front_split_ratio*len(featured_list))+100):
-	abs_dict=featured_list[i]['abs']
-	body_dict=featured_list[i]['body']
-	# max_conf_linear=0
-	max_conf_rbf=0
-	# pred_dict_linear={}
-	pred_dict_rbf={}
-	for a_key_1 in abs_dict.keys():
-		for a_key_2 in abs_dict.keys():
-			if a_key_1==a_key_2:
-				continue
-			if not a_key_1 in word_list or not a_key_2 in word_list:
-				continue
-			# pred_input=np.array([[key_phrase[word_list.index(a_key_1)],abs_dict[a_key_1][0],abs_dict[a_key_1][1]-abs_dict[a_key_1][0],abs_dict[a_key_1][2],idf[word_list.index(a_key_1)],centrality[a_key_1]],[key_phrase[word_list.index(a_key_2)],abs_dict[a_key_2][0],abs_dict[a_key_2][1]-abs_dict[a_key_2][0],abs_dict[a_key_2][2],idf[word_list.index(a_key_2)],centrality[a_key_2]]])
-			# pred_saliency_1=list(s_clf.predict(pred_input))[0]
-			# pred_saliency_2=list(s_clf.predict(pred_input))[1]
-			for b_key in word_list:
-				if a_key_1==b_key or a_key_2==b_key:
+confidence=0.0
+while confidence<1.0:
+	tp_rbf=0.0
+	fp_rbf=0.0
+	fn_rbf=0.0
+	for i in range(int(front_split_ratio*len(featured_list)),int(end_split_ratio*len(featured_list))):
+		abs_dict=featured_list[i]['abs']
+		body_dict=featured_list[i]['body']
+		max_conf_rbf=0
+		pred_dict_rbf={}
+		abs_key_list=list(abs_dict.keys())
+		for i1 in range(0,len(abs_key_list)):
+			for i2 in range(i1,len(abs_key_list)):
+				a_key_1=abs_key_list[i1]
+				a_key_2=abs_key_list[i2]
+				if a_key_1==a_key_2:
 					continue
-				count+=1
-				# if count>20000:
-				# 	outlog=random.sample(log_list,100)
-				# 	for l in outlog:
-				# 		print(l[0],l[1],l[2],l[3],l[4])
-				# 	sys.exit(0)
-				label=0
-				if b_key in body_dict.keys():
-					label=1
-				else:
-					label=0
-				_list=[dev_mat[word_list.index(a_key_1)][word_list.index(b_key)],dev_mat[word_list.index(a_key_2)][word_list.index(b_key)]]
-				_list.extend(word2tvec[a_key_1])
-				_list.extend(word2tvec[a_key_2])
-				# _list.append(label)
-				print(count,a_key_1,a_key_2,b_key,label)
-				sample_input=np.array([_list])
-
-				# pred_label_linear=list(clf_lr.predict(sample_input))[0]
-				# if pred_label_linear==1:
-				# 	if b_key in pred_dict_linear.keys():
-				# 		pred_dict_linear[b_key]+=1.0
-				# 		if pred_dict_linear[b_key]>max_conf_linear:
-				# 			max_conf_linear=pred_dict_linear[b_key]
-				# 	else:
-				# 		pred_dict_linear[b_key]=1.0
-				# 		if pred_dict_linear[b_key]>max_conf_linear:
-				# 			max_conf_linear=pred_dict_linear[b_key]
-
-				pred_label_rbf=list(clf_sgd.predict(sample_input))[0]
-				log_list.append([pred_label_rbf,label,a_key_1,a_key_2,b_key])
-				if pred_label_rbf==1:
-					if b_key in pred_dict_rbf.keys():
-						pred_dict_rbf[b_key]+=1.0
-						if pred_dict_rbf[b_key]>max_conf_rbf:
-							max_conf_rbf=pred_dict_rbf[b_key]
-					else:
-						pred_dict_rbf[b_key]=1.0
-						if pred_dict_rbf[b_key]>max_conf_rbf:
-							max_conf_rbf=pred_dict_rbf[b_key]
-
-	# for key in pred_dict_linear.keys():
-	# 	pred_dict_linear[key]/=max_conf_linear
-
-	for key in pred_dict_rbf.keys():
-		pred_dict_rbf[key]/=max_conf_rbf
-
-	# pred_set_linear=set()
-	pred_set_rbf=set()
-
-	# for key in pred_dict_linear.keys():
-	# 	if pred_dict_linear[key]>confidence:
-	# 		pred_set_linear.add(key)
-
-	for key in pred_dict_rbf.keys():
-		if pred_dict_rbf[key]>confidence:
-			pred_set_rbf.add(key)
-
-	real_set=set(body_dict.keys())-set(abs_dict.keys())
-	# tp_linear+=len(pred_set_linear&real_set)
-	# fp_linear+=len(pred_set_linear-(pred_set_linear&real_set))
-	# fn_linear+=len(real_set-(real_set&pred_set_linear))
-	tp_rbf+=len(pred_set_rbf&real_set)
-	fp_rbf+=len(pred_set_rbf-(pred_set_rbf&real_set))
-	fn_rbf+=len(real_set-(real_set&pred_set_rbf))
-
-
-# P=tp_linear/(tp_linear+fp_linear)
-# R=tp_linear/(tp_linear+fn_linear)
-# F1=2*P*R/(P+R)
-
-# print(P,R,F1)
-
-P=tp_rbf/(tp_rbf+fp_rbf)
-R=tp_rbf/(tp_rbf+fn_rbf)
-F1=2*P*R/(P+R)
-print(count)
-
-f=open("/home/ubuntu/results/coclf/sgd_test_log.txt","a")
-f.write(str(confidence)+","+str(P)+","+str(R)+","+str(F1)+"\n")
-f.close()
+				if not a_key_1 in word_list or not a_key_2 in word_list:
+					continue
+				for b_key in word_list:
+					if a_key_1==b_key or a_key_2==b_key:
+						continue
+					a1_tvec=b_word2tvec[a_key_1]
+					a2_tvec=b_word2tvec[a_key_2]
+					b_tvec=b_word2tvec[b_key]
+					disa1a2=tree_distance(a1_tvec,a2_tvec)
+					disa1b=tree_distance(a1_tvec,b_tvec)
+					disa2b=tree_distance(a2_tvec,b_tvec)
+					try:
+						_list=[dev_mat[word_list.index(a_key_1)][word_list.index(b_key)],dev_mat[word_list.index(a_key_2)][word_list.index(b_key)]]
+					except:
+						_list=[0.0,0.0]
+					_list.extend([disa1a2,disa1b,disa2b])
+					sample_input=np.array([_list])
+					pred_label_rbf=list(clf_sgd.predict(sample_input))[0]
+					if pred_label_rbf==1:
+						if b_key in pred_dict_rbf.keys():
+							pred_dict_rbf[b_key]+=1.0
+							if pred_dict_rbf[b_key]>max_conf_rbf:
+								max_conf_rbf=pred_dict_rbf[b_key]
+						else:
+							pred_dict_rbf[b_key]=1.0
+							if pred_dict_rbf[b_key]>max_conf_rbf:
+								max_conf_rbf=pred_dict_rbf[b_key]
+		for key in pred_dict_rbf.keys():
+			pred_dict_rbf[key]/=max_conf_rbf
+		pred_set_rbf=set()
+		for key in pred_dict_rbf.keys():
+			if pred_dict_rbf[key]>confidence:
+				pred_set_rbf.add(key)
+		real_set=set(body_dict.keys())-set(abs_dict.keys())
+		tp_rbf+=len(pred_set_rbf&real_set)
+		fp_rbf+=len(pred_set_rbf-(pred_set_rbf&real_set))
+		fn_rbf+=len(real_set-(real_set&pred_set_rbf))
+	try:
+		P=tp_rbf/(tp_rbf+fp_rbf)
+	except:
+		P=0.0
+	try:
+		R=tp_rbf/(tp_rbf+fn_rbf)
+	except:
+		R=0.0
+	try:
+		F1=2*P*R/(P+R)
+	except:
+		F1=0.0
+	f=open("/home/ubuntu/results/coclf/sgd_test_log_plusdis_minusvec.txt","a")
+	f.write(str(confidence)+","+str(P)+","+str(R)+","+str(F1)+"\n")
+	f.close()
+	confidence+=0.1
