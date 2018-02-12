dd0d9b1754d31d1409b8deeaee0c729f07b73e00
NiMaZi <zhengyl940425@gmail.com>
2018-02-12 15:01:47 +0100

    1

--- saliency/cooc_ncit.py
+++ saliency/cooc_ncit.py
@@ -1,16 +1,12 @@
-# import sys
-# import csv
+import sys
+import csv
 import json
-# from scipy.sparse import lil_matrix
+from scipy.sparse import lil_matrix
 
 f=open("/home/ubuntu/results_new/ontology/wordlist_id.json",'r',encoding='utf-8')
 wlid=json.load(f)
 f.close()
 
-f=open("/home/ubuntu/results_new/ontology/word_list.json",'r',encoding='utf-8')
-wl=json.load(f)
-f.close()
+dev_mat=lil_matrix((len(wlid),len(wlid)))
 
-for i in range(0,len(wl)):
-	if not wl[i]==wlid[i+1].split('#')[1]:
-		print(wl[i],wlid[i+1])
+print(type(dev_mat))
