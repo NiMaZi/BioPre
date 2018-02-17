63da0746366034d85e991dbcce203804d92123d7
NiMaZi <zhengyl940425@gmail.com>
2018-02-17 08:27:30 +0100

    try

--- subset.py
+++ subset.py
@@ -5,37 +5,49 @@ volume=12935
 
 word_set=set()
 for i in range(0,volume):
-	f=open("/home/ubuntu/thesiswork/kdata/abs"+str(i)+".csv",'r',encoding='utf-8')
-	rd=csv.reader(f)
-	for item in rd:
-		if item[1]=="ConceptCode":
-			continue
-		word_set.add(item[1])
-	f.close()
+	try:
+		f=open("/home/ubuntu/thesiswork/kdata/abs"+str(i)+".csv",'r',encoding='utf-8')
+		rd=csv.reader(f)
+		for item in rd:
+			if item[1]=="ConceptCode":
+				continue
+			word_set.add(item[1])
+		f.close()
+	except:
+		pass
 
-	f=open("/home/ubuntu/thesiswork/kdata/body"+str(i)+".csv",'r',encoding='utf-8')
-	rd=csv.reader(f)
-	for item in rd:
-		if item[1]=="ConceptCode":
-			continue
-		word_set.add(item[1])
-	f.close()
+	try:
+		f=open("/home/ubuntu/thesiswork/kdata/body"+str(i)+".csv",'r',encoding='utf-8')
+		rd=csv.reader(f)
+		for item in rd:
+			if item[1]=="ConceptCode":
+				continue
+			word_set.add(item[1])
+		f.close()
+	except:
+		pass
 
-	f=open("/home/ubuntu/thesiswork/kdata/title"+str(i)+".csv",'r',encoding='utf-8')
-	rd=csv.reader(f)
-	for item in rd:
-		if item[1]=="ConceptCode":
-			continue
-		word_set.add(item[1])
-	f.close()
+	try:
+		f=open("/home/ubuntu/thesiswork/kdata/title"+str(i)+".csv",'r',encoding='utf-8')
+		rd=csv.reader(f)
+		for item in rd:
+			if item[1]=="ConceptCode":
+				continue
+			word_set.add(item[1])
+		f.close()
+	except:
+		pass
 
-	f=open("/home/ubuntu/thesiswork/kdata/keywords"+str(i)+".csv",'r',encoding='utf-8')
-	rd=csv.reader(f)
-	for item in rd:
-		if item[1]=="ConceptCode":
-			continue
-		word_set.add(item[1])
-	f.close()
+	try:
+		f=open("/home/ubuntu/thesiswork/kdata/keywords"+str(i)+".csv",'r',encoding='utf-8')
+		rd=csv.reader(f)
+		for item in rd:
+			if item[1]=="ConceptCode":
+				continue
+			word_set.add(item[1])
+		f.close()
+	except:
+		pass
 
 print(len(word_set))
 word_list=list(word_set)
