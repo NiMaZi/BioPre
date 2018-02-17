d94d8bae16f9362677692f277be3590c2abc3281
NiMaZi <zhengyl940425@gmail.com>
2018-02-17 09:23:25 +0100

    set mem

--- models/entity_cooc.c
+++ models/entity_cooc.c
@@ -3,6 +3,11 @@
 
 int main()
 {
-	unsigned short mat[48155][48155]
+	unsigned short mat[48155][48155];
+	for(int i=0;i<48155;i++){
+		for(int j=0;j<48155;j++){
+			mat[i][j]=0;
+		}
+	}
 	return 0;
 }
