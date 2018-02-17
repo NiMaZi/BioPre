61513797d969a497766062cca3791ffb3c39c52c
NiMaZi <zhengyl940425@gmail.com>
2018-02-17 01:46:44 +0100

    make subset

---
+++ subset.py
@@ -0,0 +1,40 @@
+import csv
+import json
+
+volume=12
+
+word_set=set()
+for i in range(0,volume):
+	f=open("/home/ubuntu/thesiswork/kdata/abs"+str(i)+".csv",'r',encoding='utf-8')
+	rd=csv.reader(f)
+	for item in rd:
+		if item[1]=="ConceptCode":
+			continue
+		word_set.add(item[1])
+	f.close()
+
+	f=open("/home/ubuntu/thesiswork/kdata/body"+str(i)+".csv",'r',encoding='utf-8')
+	rd=csv.reader(f)
+	for item in rd:
+		if item[1]=="ConceptCode":
+			continue
+		word_set.add(item[1])
+	f.close()
+
+	f=open("/home/ubuntu/thesiswork/kdata/title"+str(i)+".csv",'r',encoding='utf-8')
+	rd=csv.reader(f)
+	for item in rd:
+		if item[1]=="ConceptCode":
+			continue
+		word_set.add(item[1])
+	f.close()
+
+	f=open("/home/ubuntu/thesiswork/kdata/keywords"+str(i)+".csv",'r',encoding='utf-8')
+	rd=csv.reader(f)
+	for item in rd:
+		if item[1]=="ConceptCode":
+			continue
+		word_set.add(item[1])
+	f.close()
+
+print(word_set)
