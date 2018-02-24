ae36d1cb05d45f681188220a652549ef880ad0bc
NiMaZi <zhengyl940425@gmail.com>
2018-02-24 14:56:00 +0100

    new jacquard

--- models/e2vtrain.py
+++ models/e2vtrain.py
@@ -2,6 +2,13 @@ from gensim.models import word2vec
 
 sentences=word2vec.LineSentence("/home/ubuntu/thesiswork/source/corpus/corpus5000.txt")
 
+f=open("/home/ubuntu/results/statistics/tf_all.json",'r')
+tf_all=json.load(f)
+f.close()
+tf_all_com={}
+for k in tf_all.keys():
+    tf_all_com[k]=tf_all[k]+1e-5
+
 model=word2vec.Word2Vec(sentences,sg=0,size=200,window=10,min_count=0,sample=1e-3,hs=0,negative=1,sorted_vocab=1)
 path="/home/ubuntu/results/models/e2v_sg_5000.model"
 model.save(path)
--- ontology/jaccard.py
+++ ontology/jaccard.py
@@ -1,25 +1,24 @@
 from scipy.sparse import lil_matrix, csc_matrix, save_npz, load_npz
 
-KG=load_npz("/home/ubuntu/results/ontology/KG_raw.npz")
+KG_raw=load_npz("/home/ubuntu/results/ontology/KG_raw.npz")
+KG=KG_raw.tolil()
 
 Jaccard=lil_matrix((133610,133610))
-common_neighbor=lil_matrix((133610,133610))
 
 for i in range(0,133610):
     for j in range(i,133610):
         if i==j:
-            jacc=1.0
-            Jaccard[i,i]=jacc
+            continue
         else:
-            u=KG.getrow(i)
-            v=KG.getrow(j)
+            u=KG.getrowview(i)
+            v=KG.getrowview(j)
             set_u=set(u.nonzero()[1])
             set_v=set(u.nonzero()[1])
             comm=float(len(set_u&set_v))
+            if not comm:
+                continue
             jacc=float(len(set_u&set_v))/float(len(set_u|set_v))
             Jaccard[i,j]=jacc
             Jaccard[j,i]=jacc
-            common_neighbor[i,j]=comm
-            common_neighbor[j,i]=comm
 
 save_npz("/home/ubuntu/results/ontology/Jaccard.npz",Jaccard.tocsc())
