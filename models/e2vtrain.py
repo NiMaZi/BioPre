b3d3136bb7471044a1b38b68fd632eebd565509e
NiMaZi <zhengyl940425@gmail.com>
2018-02-24 15:11:32 +0100

    e2v

--- models/e2vtrain.py
+++ models/e2vtrain.py
@@ -1,3 +1,4 @@
+import json
 from gensim.models import word2vec
 
 sentences=word2vec.LineSentence("/home/ubuntu/thesiswork/source/corpus/corpus5000.txt")
@@ -5,14 +6,20 @@ sentences=word2vec.LineSentence("/home/ubuntu/thesiswork/source/corpus/corpus500
 f=open("/home/ubuntu/results/statistics/tf_all.json",'r')
 tf_all=json.load(f)
 f.close()
+
 tf_all_com={}
 for k in tf_all.keys():
-    tf_all_com[k]=tf_all[k]+1e-5
+	_k=k.split('#')[1]
+    tf_all_com[_k]=tf_all[k]+1e-10
 
-model=word2vec.Word2Vec(sentences,sg=0,size=200,window=10,min_count=0,sample=1e-3,hs=0,negative=1,sorted_vocab=1)
-path="/home/ubuntu/results/models/e2v_sg_5000.model"
+model=word2vec.Word2Vec(sg=0,size=200,window=10,min_count=0,sample=1e-3,hs=0,negative=1,sorted_vocab=1)
+model.build_vocab_from_freq(tf_all_com)
+model.train(sentences,total_examples=5000,epochs=100)
+path="/home/ubuntu/results/models/e2v_sg_5000_e100.model"
 model.save(path)
 
-model=word2vec.Word2Vec(sentences,sg=1,size=200,window=10,min_count=0,sample=1e-3,hs=0,negative=1,sorted_vocab=1)
-path="/home/ubuntu/results/models/e2v_cbow_5000.model"
+model=word2vec.Word2Vec(sg=1,size=200,window=10,min_count=0,sample=1e-3,hs=0,negative=1,sorted_vocab=1)
+model.build_vocab_from_freq(tf_all_com)
+model.train(sentences,total_examples=5000,epochs=100)
+path="/home/ubuntu/results/models/e2v_cbow_5000_e100.model"
 model.save(path)
