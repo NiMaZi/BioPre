823bc27793fd1ca86df5100d745807503b4a1585
NiMaZi <zhengyl940425@gmail.com>
2018-02-28 12:05:54 +0100

    e2v10000

--- models/e2vtrain.py
+++ models/e2vtrain.py
@@ -1,7 +1,7 @@
 import json
 from gensim.models import word2vec
 
-sentences=word2vec.LineSentence("/home/ubuntu/thesiswork/source/corpus/fullcorpus5000.txt")
+sentences=word2vec.LineSentence("/home/ubuntu/thesiswork/source/corpus/fullcorpus10000.txt")
 # sentences=word2vec.LineSentence("/home/ubuntu/results/hivo_corpus.txt")
 
 f=open("/home/ubuntu/results/statistics/tf_all.json",'r')
@@ -10,46 +10,47 @@ f.close()
 
 tf_all_com={}
 for k in tf_all.keys():
-	tf_all_com[k]=tf_all[k]+1e-10
+	_k=k.split("#")[1]
+	tf_all_com[_k]=tf_all[k]+1e-10
 
-model=word2vec.Word2Vec(sg=0,size=128,window=10,min_count=0,sample=1e-3,hs=0,negative=1,sorted_vocab=1)
-model.build_vocab_from_freq(tf_all_com)
-model.train(sentences,total_examples=10000,epochs=100)
-path="/home/ubuntu/results/models/e2v_sg_5000_e100_d128.model"
-# path="/home/ubuntu/results/e2v_sg_e100.model"
-model.save(path)
+# model=word2vec.Word2Vec(sg=0,size=128,window=10,min_count=0,sample=1e-3,hs=0,negative=1,sorted_vocab=1)
+# model.build_vocab_from_freq(tf_all_com)
+# model.train(sentences,total_examples=10000,epochs=100)
+# path="/home/ubuntu/results/models/e2v_sg_5000_e100_d128.model"
+# # path="/home/ubuntu/results/e2v_sg_e100.model"
+# model.save(path)
 
 model=word2vec.Word2Vec(sg=0,size=64,window=10,min_count=0,sample=1e-3,hs=0,negative=1,sorted_vocab=1)
 model.build_vocab_from_freq(tf_all_com)
 model.train(sentences,total_examples=10000,epochs=100)
-path="/home/ubuntu/results/models/e2v_sg_5000_e100_d64.model"
+path="/home/ubuntu/results/models/e2v_sg_10000_e100_d64.model"
 # path="/home/ubuntu/results/e2v_sg_e100.model"
 model.save(path)
 
-model=word2vec.Word2Vec(sg=0,size=32,window=10,min_count=0,sample=1e-3,hs=0,negative=1,sorted_vocab=1)
-model.build_vocab_from_freq(tf_all_com)
-model.train(sentences,total_examples=10000,epochs=100)
-path="/home/ubuntu/results/models/e2v_sg_5000_e100_d32.model"
-# path="/home/ubuntu/results/e2v_sg_e100.model"
-model.save(path)
+# model=word2vec.Word2Vec(sg=0,size=32,window=10,min_count=0,sample=1e-3,hs=0,negative=1,sorted_vocab=1)
+# model.build_vocab_from_freq(tf_all_com)
+# model.train(sentences,total_examples=10000,epochs=100)
+# path="/home/ubuntu/results/models/e2v_sg_5000_e100_d32.model"
+# # path="/home/ubuntu/results/e2v_sg_e100.model"
+# model.save(path)
 
-model=word2vec.Word2Vec(sg=1,size=128,window=10,min_count=0,sample=1e-3,hs=0,negative=1,sorted_vocab=1)
-model.build_vocab_from_freq(tf_all_com)
-model.train(sentences,total_examples=10000,epochs=100)
-path="/home/ubuntu/results/models/e2v_cbow_5000_e100_d128.model"
-# path="/home/ubuntu/results/e2v_cbow_e100.model"
-model.save(path)
+# model=word2vec.Word2Vec(sg=1,size=128,window=10,min_count=0,sample=1e-3,hs=0,negative=1,sorted_vocab=1)
+# model.build_vocab_from_freq(tf_all_com)
+# model.train(sentences,total_examples=10000,epochs=100)
+# path="/home/ubuntu/results/models/e2v_cbow_5000_e100_d128.model"
+# # path="/home/ubuntu/results/e2v_cbow_e100.model"
+# model.save(path)
 
 model=word2vec.Word2Vec(sg=1,size=64,window=10,min_count=0,sample=1e-3,hs=0,negative=1,sorted_vocab=1)
 model.build_vocab_from_freq(tf_all_com)
 model.train(sentences,total_examples=10000,epochs=100)
-path="/home/ubuntu/results/models/e2v_cbow_5000_e100_d64.model"
+path="/home/ubuntu/results/models/e2v_cbow_10000_e100_d64.model"
 # path="/home/ubuntu/results/e2v_cbow_e100.model"
 model.save(path)
 
-model=word2vec.Word2Vec(sg=1,size=32,window=10,min_count=0,sample=1e-3,hs=0,negative=1,sorted_vocab=1)
-model.build_vocab_from_freq(tf_all_com)
-model.train(sentences,total_examples=10000,epochs=100)
-path="/home/ubuntu/results/models/e2v_cbow_5000_e100_d32.model"
-# path="/home/ubuntu/results/e2v_cbow_e100.model"
-model.save(path)
+# model=word2vec.Word2Vec(sg=1,size=32,window=10,min_count=0,sample=1e-3,hs=0,negative=1,sorted_vocab=1)
+# model.build_vocab_from_freq(tf_all_com)
+# model.train(sentences,total_examples=10000,epochs=100)
+# path="/home/ubuntu/results/models/e2v_cbow_5000_e100_d32.model"
+# # path="/home/ubuntu/results/e2v_cbow_e100.model"
+# model.save(path)
