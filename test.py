7d6ea45e8658c3d97048c0218d597e5809c9ac6e
NiMaZi <zhengyl940425@gmail.com>
2018-03-11 14:48:07 +0100

    test

--- test.py
+++ test.py
@@ -1,43 +1,51 @@
+import numpy as np
+from keras.models import Sequential,load_model
+from keras.layers import Dense,LSTM,Bidirectional,Masking,BatchNormalization
+from keras.callbacks import EarlyStopping,TerminateOnNaN
+
+dim=128
+maxlen=400
+
+model=Sequential()
+model.add(Masking(mask_value=0.0,input_shape=(maxlen,dim)))
+model.add(BatchNormalization())
+model.add(Bidirectional(LSTM(dim,return_sequences=False,dropout=0.5,activation="relu"),merge_mode='ave'))
+model.compile(optimizer='nadam',loss='binary_crossentropy')
+
+sample=1000
+X=np.random.rand(sample,maxlen,dim)
+y=np.random.rand(sample,dim)
+X[:,3,:]=0.0
+X[5,5,:]=0.0
+X[1,5,:]=0.0
+
+terminateonnan=TerminateOnNaN()
+early_stopping=EarlyStopping(monitor='loss',patience=10)
+early_stopping_val=EarlyStopping(monitor='val_loss',patience=10)
+model.fit(X,y,batch_size=32,epochs=10,shuffle=True,validation_split=0.1,callbacks=[early_stopping,early_stopping_val,terminateonnan])
+
+
 # import numpy as np
-# from keras.models import Sequential, load_model
-# from keras.layers import LSTM, Bidirectional, Masking
+# from keras import optimizers
+# from keras.models import Sequential,load_model
+# from keras.layers import Dense,Dropout,BatchNormalization
+# from keras.callbacks import EarlyStopping
+
+# dim=8192
+# hrate=1.5
+# drate=0.5
+
+# myAct='relu'
+# model=Sequential()
+# model.add(Dense(int(dim*hrate),input_dim=dim,activation=myAct))
+# model.add(BatchNormalization())
+# model.add(Dropout(drate))
+# model.add(Dense(dim,activation=myAct))
+# model.compile(optimizer='Nadam',loss='binary_crossentropy')
 
 # sample=1024
-# length=290
-# dim=256
-
-# model=Sequential()
-# model.add(Masking(mask_value=0.0,input_shape=(length,dim)))
-# model.add(Bidirectional(LSTM(dim,return_sequences=False,activation="relu"),merge_mode='ave'))
-# model.compile(optimizer='nadam',loss='binary_crossentropy')
-
-# X=np.random.rand(sample,length,dim)
+# X=np.random.rand(sample,dim)
 # y=np.random.rand(sample,dim)
 
-# model.fit(X,y,batch_size=256,epochs=10)
-
-
-import numpy as np
-from keras import optimizers
-from keras.models import Sequential,load_model
-from keras.layers import Dense,Dropout,BatchNormalization
-from keras.callbacks import EarlyStopping
-
-dim=8192
-hrate=1.5
-drate=0.5
-
-myAct='relu'
-model=Sequential()
-model.add(Dense(int(dim*hrate),input_dim=dim,activation=myAct))
-model.add(BatchNormalization())
-model.add(Dropout(drate))
-model.add(Dense(dim,activation=myAct))
-model.compile(optimizer='Nadam',loss='binary_crossentropy')
-
-sample=1024
-X=np.random.rand(sample,dim)
-y=np.random.rand(sample,dim)
-
-early_stopping=EarlyStopping(monitor='loss',patience=10)
-model.fit(X,y,batch_size=256,epochs=100,shuffle=True,callbacks=[early_stopping])
+# early_stopping=EarlyStopping(monitor='loss',patience=10)
+# model.fit(X,y,batch_size=256,epochs=100,shuffle=True,callbacks=[early_stopping])
