a0b1864f7d33007a379f1a15e18d06d507807c13
NiMaZi <zhengyl940425@gmail.com>
2018-03-13 15:57:36 +0100

    run gpu score

--- models/gpu_score.py
+++ models/gpu_score.py
@@ -1,13 +1,14 @@
 import os
 import json
 import numpy as np
+from scipy.spatial.distance import cosine
 from keras.models import Sequential, load_model
 from keras.layers import LSTM,Bidirectional,Masking,BatchNormalization
 from keras.callbacks import EarlyStopping
 from gensim.models import word2vec as w2v
 
 dim=128
-maxlen=1024
+maxlen=512
 volume=1000
 homedir=os.environ['HOME']
 
@@ -49,16 +50,28 @@ def load_corpus(_path):
         corpus.append(_p)
     return corpus[volume:volume+200]
 
-path=homedir+"/thesiswork/corpus/fullcorpusall.txt"
+path=homedir+"/thesiswork/source/corpus/fullcorpusall.txt"
 corpus=load_corpus(path)
+
+def find_match(vec,num):
+    min_dis=np.inf
+    min_word=None
+    for w in word_list:
+        dis=cosine(vec,get_emb(w.split('#')[1]))
+        if dis<min_dis:
+            min_dis=dis
+            min_word=w.split('#')[1]
+    return min_word,min_dis
 
 def test_on_data(_corpus,_maxlen,_model):
     i=0
     comp_vec=[0.0 for i in range(0,128)]
     ndata=[]
+    hit=0.0
     while(i<len(_corpus)-1):
-        print("testing on doc "+str(i))
         _body=_corpus[i]
+        i+=1
+        _rbody=set(_corpus[i])
         b_emb=[]
         if len(_body)<_maxlen:
             for w in _body:
@@ -67,7 +80,11 @@ def test_on_data(_corpus,_maxlen,_model):
                 b_emb.append(comp_vec)
             ndata=np.array([b_emb])
             y_out=model.predict(ndata)
+            match,dis=find_match(y_out[0],1)
+            if match in _rbody:
+                hit+=1.0
         else:
+            all_match=[]
             for j in range(0,len(_body)-_maxlen+1):
                 b_emb=[]
                 for wj in range(0,_maxlen):
@@ -75,9 +92,20 @@ def test_on_data(_corpus,_maxlen,_model):
                     b_emb.append(get_emb(w))
                 ndata=[b_emb]
                 y_out=model.predict(ndata)
-        i+=2
-        print(len(y_out[0]),y_out[0])
+                match,dis=find_match(y_out[0],1)
+                all_match.append((match,dis))
+            best_match=min(all_match,key=lambda x:x[1])[0]
+            if best_match in _rbody:
+                hit+=1.0
+        i+=1
+    hit/=len(_corpus)/2.0
+    return hit
 
-
-model=load_model(homedir+"/results/models/BiLSTM0.h5")
-test_on_data(corpus,maxlen,model)
+mod_no=0
+logf=open(homedir+"/results/logs/BiLSTMGPU_log.txt",'a')
+while mod_no<=90:
+    model=load_model(homedir+"/results/models/BiLSTMGPU"+str(mod_no)+".h5")
+    hit=test_on_data(corpus,maxlen,model)
+    logf.write("%d,%.3f"%(mod_no,hit))
+    mod_no+=10
+logf.close()
