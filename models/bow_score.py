357f31aaaeb96dbb680c4390d3a7351f94935174
NiMaZi <zhengyl940425@gmail.com>
2018-03-20 15:26:34 +0100

    loop

--- models/bow_score.py
+++ models/bow_score.py
@@ -109,15 +109,16 @@ def test_on_doc_S3(_model,_volume,_threshold=0.0):
 if __name__=="__main__":
 	homedir=os.environ['HOME']
 	logf=open(homedir+"/results/logs/bow_score.txt",'a')
-	model_name="MLPsparse_1hidden_2"
-	volume=100
-	logf.write("%s,%d\n"%(model_name,volume))
-	model=get_model_S3(model_name)
-	# model=get_model_local(homedir+"/results/models/"+model_name+".h5")
-	threshold=0.0
-	while threshold<0.15:
-		P,R,F1=test_on_doc_S3(model,volume,threshold)
-		logf.write("%.3f,%.3f,%.3f,%.3f\n"%(threshold,P,R,F1))
-		print("%.3f,%.3f,%.3f,%.3f"%(threshold,P,R,F1))
-		threshold+=0.01
+	for i in range(3,8):
+		model_name="MLPsparse_1hidden_"+str(i)
+		volume=100
+		logf.write("%s,%d\n"%(model_name,volume))
+		model=get_model_S3(model_name)
+		# model=get_model_local(homedir+"/results/models/"+model_name+".h5")
+		threshold=0.0
+		while threshold<0.15:
+			P,R,F1=test_on_doc_S3(model,volume,threshold)
+			logf.write("%.3f,%.3f,%.3f,%.3f\n"%(threshold,P,R,F1))
+			print("%.3f,%.3f,%.3f,%.3f"%(threshold,P,R,F1))
+			threshold+=0.01
 	logf.close()
