dd3d6cb294f522ed0285f46ecd03cdaa91c49f58
NiMaZi <zhengyl940425@gmail.com>
2018-03-25 00:54:46 +0100

    bow score

--- models/bow_score.py
+++ models/bow_score.py
@@ -15,11 +15,10 @@ def get_bucket():
 
 def load_sups():
 	homedir=os.environ['HOME']
-	f=open(homedir+"/results/ontology/full_word_list.json",'r')
-	word_list=json.load(f)
+	f=open(homedir+"/results/ontology/ConCode2Vid.json",'r')
+	cc2vid=json.load(f)
 	f.close()
-	prefix='http://ncicb.nci.nih.gov/xml/owl/EVS/Thesaurus.owl#'
-	return prefix,word_list[1:]
+	return cc2vid
 
 def get_model_S3(_name):
 	bucket=get_bucket()
@@ -38,13 +37,13 @@ def get_model_local(path):
 def test_on_doc_S3(_model,_volume,_threshold=0.0):
 	homedir=os.environ['HOME']
 	bucket=get_bucket()
-	prefix,word_list=load_sups()
+	cc2vid=load_sups()
 	sample_list=[]
 	P_all=0.0
 	R_all=0.0
 	all_count=0.0
 	for i in range(0,_volume):
-		abs_vec=[0.0 for i in range(0,len(word_list))]
+		abs_vec=[0.0 for i in range(0,len(cc2vid))]
 		abs_count=0.0
 		bucket.download_file("yalun/annotated_papers_meta/abs"+str(i)+".csv",homedir+"/temp/tmp.csv")
 		with open(homedir+"/temp/tmp.csv",'r',encoding='utf-8') as cf:
@@ -53,11 +52,11 @@ def test_on_doc_S3(_model,_volume,_threshold=0.0):
 				if item[0]=="Mention":
 					continue
 				abs_count+=1.0
-				abs_vec[word_list.index(prefix+item[1])]+=1.0
+				abs_vec[cc2vid[item[1]]]+=1.0
 		if not abs_count:
 			continue
 		abs_vec=list(np.array(abs_vec)/abs_count)
-		body_vec=[0.0 for i in range(0,len(word_list))]
+		body_vec=[0.0 for i in range(0,len(cc2vid))]
 		body_count=0.0
 		bucket.download_file("yalun/annotated_papers_meta/body"+str(i)+".csv",homedir+"/temp/tmp.csv")
 		with open(homedir+"/temp/tmp.csv",'r',encoding='utf-8') as cf:
@@ -66,14 +65,14 @@ def test_on_doc_S3(_model,_volume,_threshold=0.0):
 				if item[0]=="Mention":
 					continue
 				body_count+=1.0
-				body_vec[word_list.index(prefix+item[1])]+=1.0
+				body_vec[cc2vid[item[1]]]+=1.0
 		if not body_count:
 			continue
-		body_vec=list(np.array(abs_vec)/body_count)
+		body_vec=list(np.array(body_vec)/body_count)
 		sample_list=[abs_vec+body_vec]
 		N_test=np.array(sample_list)
-		X_test=N_test[:,:len(word_list)]
-		Y_test=np.ceil(N_test[:,len(word_list):])[0]
+		X_test=N_test[:,:len(cc2vid)]
+		Y_test=np.ceil(N_test[:,len(cc2vid):])[0]
 		Y_pred=_model.predict(X_test)[0]
 		Y_pred/=np.linalg.norm(Y_pred)
 		bset=set()
@@ -107,18 +106,20 @@ def test_on_doc_S3(_model,_volume,_threshold=0.0):
 	return P_all,R_all,F1
 
 if __name__=="__main__":
-	for i in range(0,50):
+	i=0
+	while i<=110
 		homedir=os.environ['HOME']
 		logf=open(homedir+"/results/logs/bow_score.txt",'a')
-		model_name="MLPsparse_2hidden_"+str(i)
-		volume=5
+		model_name="MLPsparse_1hidden_"+str(i)
+		volume=100
 		logf.write("%s,%d\n"%(model_name,volume))
 		# model=get_model_S3(model_name)
 		model=get_model_local(homedir+"/temp/tmp_model.h5")
 		threshold=0.0
-		while threshold<1.0:
+		while threshold<0.1:
 			P,R,F1=test_on_doc_S3(model,volume,threshold)
 			logf.write("%.3f,%.3f,%.3f,%.3f\n"%(threshold,P,R,F1))
-			print("%.3f,%.3f,%.3f,%.3f"%(threshold,P,R,F1))
-			threshold+=0.1
+			# print("%.3f,%.3f,%.3f,%.3f"%(threshold,P,R,F1))
+			threshold+=0.01
 		logf.close()
+		i+=10
