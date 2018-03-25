a71b593b13108c118e6c4bc0f27a474dab051cde
NiMaZi <zhengyl940425@gmail.com>
2018-03-25 17:06:02 +0200

    0->130

--- models/bow_score_sem.py
+++ models/bow_score_sem.py
@@ -107,7 +107,7 @@ def test_on_doc_S3(_model,_volume,_threshold=0.0):
 
 if __name__=="__main__":
 	i=0
-	while i<=10:
+	while i<=130:
 		homedir=os.environ['HOME']
 		logf=open(homedir+"/results/logs/bow_score_sem.txt",'a')
 		model_name="MLPsparse_1hidden_sem_"+str(i)
@@ -119,7 +119,7 @@ if __name__=="__main__":
 		while threshold<0.1:
 			P,R,F1=test_on_doc_S3(model,volume,threshold)
 			logf.write("%.3f,%.3f,%.3f,%.3f\n"%(threshold,P,R,F1))
-			print("%.3f,%.3f,%.3f,%.3f"%(threshold,P,R,F1))
+			# print("%.3f,%.3f,%.3f,%.3f"%(threshold,P,R,F1))
 			threshold+=0.01
 		logf.close()
 		i+=10
