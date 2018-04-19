301bdcda19aab884bb5bf828c4ebafed59e4df00
NiMaZi <zhengyl940425@gmail.com>
2018-04-19 10:57:04 +0200

    ep opt

--- models/bow_ep.py
+++ models/bow_ep.py
@@ -28,6 +28,9 @@ def build_model(_input_dim=133609,_hidden_dim=512,_drate=0.5):
 	model.add(Dense(16,activation='relu'))
 	model.compile(optimizer='nadam',loss='binary_crossentropy')
 	return model
+	
+def get_model_local(path):
+	return load_model(path)
 
 def train_on_batch_S3(_model,_source,_volume,_bcount,_batch,_mbatch,_epochs=5):
 	early_stopping=EarlyStopping(monitor='loss',patience=2)
@@ -239,7 +242,7 @@ def train_on_batch_S3(_model,_source,_volume,_bcount,_batch,_mbatch,_epochs=5):
 			bucket.put_object(Body=updata,Key="yalun/results/models/MLPsparse_1hidden_cluster_opt.h5")
 			s3f.close()
 			logf=open(homedir+"/results/logs/bow_training_log_cluster.txt",'a')
-			logf.write("ep_opt,%s,%d\n"%(str(_source),batch_count))
+			logf.write("ep_opt,%s,%d,%d,%d\n"%(str(_source),_epochs,_mbatch,batch_count))
 			logf.close()
 			batch_count+=1
 			sample_list=[]
@@ -258,7 +261,7 @@ def train_on_batch_S3(_model,_source,_volume,_bcount,_batch,_mbatch,_epochs=5):
 		bucket.put_object(Body=updata,Key="yalun/results/models/MLPsparse_1hidden_cluster_opt.h5")
 		s3f.close()
 		logf=open(homedir+"/results/logs/bow_training_log_cluster.txt",'a')
-		logf.write("ep_opt,%s,%d\n"%(str(_source),batch_count))
+		logf.write("ep_opt,%s,%d,%d,%d\n"%(str(_source),_epochs,_mbatch,batch_count))
 		logf.close()
 		batch_count+=1
 	return _model,batch_count
@@ -266,5 +269,6 @@ def train_on_batch_S3(_model,_source,_volume,_bcount,_batch,_mbatch,_epochs=5):
 if __name__=="__main__":
 	model=build_model()
 	source_key=["EEG_expansion","Dependence","Microscopy","annotated_papers_with_txt_new2"]
-	model,bcount=train_on_batch_S3(model,source_key,30000,0,272,256)
-	model,bcount=train_on_batch_S3(model,source_key,30000,0,1088,1024)
+	model,bcount=train_on_batch_S3(model,source_key,5000,0,272,256)
+	model,bcount=train_on_batch_S3(model,source_key,5000,0,1088,256)
+	model,bcount=train_on_batch_S3(model,source_key,5000,0,1088,1024)
