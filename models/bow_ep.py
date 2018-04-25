7d33bae63036e513e85793a0c84d823d90e6f247
NiMaZi <zhengyl940425@gmail.com>
2018-04-25 16:28:24 +0200

    change structure

--- models/bow_ep.py
+++ models/bow_ep.py
@@ -42,117 +42,114 @@ def train_on_batch_S3(_model,_source,_volume,_bcount,_batch,_mbatch,_epochs=5):
 	batch_count=_bcount
 	for i in range(0,_volume):
 		for j in range(0,len(_source)):
-			abs_vec=[0.0 for k in range(0,len(cc2vid))]
-			abs_count=0.0
 			try:
+				abs_vec=[0.0 for k in range(0,len(cc2vid))]
+				abs_count=0.0
 				bucket.download_file("yalun/"+_source[j]+"/abs"+str(i)+".csv",homedir+"/temp/tmp1.csv")
-			except:
-				continue
-			with open(homedir+"/temp/tmp1.csv",'r',encoding='utf-8') as cf:
-				rd=csv.reader(cf)
-				for item in rd:
-					if item[0]=="Mention":
-						continue
-					try:
-						abs_vec[cc2vid[item[1]]]+=1.0
-						abs_count+=1.0
-					except:
-						pass
-			if not abs_count:
-				continue
-			abs_vec=list(np.array(abs_vec)/abs_count)
-			body_vec=[0.0 for k in range(0,41)]
-			try:
+				with open(homedir+"/temp/tmp1.csv",'r',encoding='utf-8') as cf:
+					rd=csv.reader(cf)
+					for item in rd:
+						if item[0]=="Mention":
+							continue
+						try:
+							abs_vec[cc2vid[item[1]]]+=1.0
+							abs_count+=1.0
+						except:
+							pass
+				if not abs_count:
+					continue
+				abs_vec=list(np.array(abs_vec)/abs_count)
+				body_vec=[0.0 for k in range(0,41)]
 				bucket.download_file("yalun/"+_source[j]+"/body"+str(i)+".csv",homedir+"/temp/tmp1.csv")
+				with open(homedir+"/temp/tmp1.csv",'r',encoding='utf-8') as cf:
+					rd=csv.reader(cf)
+					for item in rd:
+						if item[0]=="Mention":
+							continue
+						if item[1]=='C38054':	#EEG
+							body_vec[0]=1.0
+						if item[1]=='C16809':	#MRI
+							body_vec[1]=1.0
+						if item[1]=='C116454':	#fMRI
+							body_vec[2]=1.0
+						if item[1]=='C116655':	#TMS
+							body_vec[3]=1.0
+						if item[1]=='C129862':	#tDCS
+							body_vec[4]=1.0
+						if item[1]=='C16811':	#MEG
+							body_vec[5]=1.0
+						if item[1]=='C78814' or item[1]=='C78815':	#SEM
+							body_vec[6]=1.0
+						if item[1]=='C78860' or item[1]=='C78813':	#TEM
+							body_vec[7]=1.0
+						if item[1]=='C17374':	#STM
+							body_vec[8]=1.0
+						if item[1]=='C78804':	#AFM
+							body_vec[9]=1.0
+						if item[1]=='C17753' or item[1]=='C122390' or item[1]=='C116477' or item[1]=='C116481':	#Confocal
+							body_vec[10]=1.0
+						if item[1]=='C93040':	#Alcohol
+							body_vec[11]=1.0
+						if item[1]=='C35386' or item[1]=='C35387' or item[1]=='C34445':	#Cannabis
+							body_vec[12]=1.0
+						if item[1]=='C34492' or item[1]=='C35389' or item[1]=='C35388':	#Cocaine
+							body_vec[13]=1.0
+						if item[1]=='C34694':	#Heroin
+							body_vec[14]=1.0
+						if item[1]=='C70989' or item[1]=='C54203' or item[1]=='C15985':	#Nicotine
+							body_vec[15]=1.0
+						if item[1]=='C73850':	#duck
+							body_vec[16]=1.0
+						if item[1]=='C91813':	#quail
+							body_vec[17]=1.0
+						if item[1]=='C91812':	#pigeon
+							body_vec[18]=1.0
+						if item[1]=='C77098':	#leghorn chicken
+							body_vec[19]=1.0
+						if item[1]=='C76362':	#broiler chicken
+							body_vec[20]=1.0
+						if item[1]=='C17611':	#Bronchoscope
+							body_vec[21]=1.0
+						if item[1]=='C17613':	#Colonoscope
+							body_vec[22]=1.0
+						if item[1]=='C17614':	#Colposcope
+							body_vec[23]=1.0
+						if item[1]=='C17616':	#Cystoscope
+							body_vec[24]=1.0
+						if item[1]=='C17620':	#Gastroscope
+							body_vec[25]=1.0
+						if item[1]=='C28167':	#Laparoscope
+							body_vec[26]=1.0
+						if item[1]=='C17618':	#Laryngoscope
+							body_vec[27]=1.0
+						if item[1]=='C85574':	#Atomic Absorption Spectroscopy
+							body_vec[28]=1.0
+						if item[1]=='C94374':	#Optical Spectroscopy
+							body_vec[29]=1.0
+						if item[1]=='C17157':	#Raman Spectroscopy
+							body_vec[30]=1.0
+						if item[1]=='C78869':	#Energy Dispersive Spectroscopy
+							body_vec[31]=1.0
+						if item[1]=='C16810':	#Magnetic Resonance Spectroscopy
+							body_vec[32]=1.0
+						if item[1]=='C62329':	#Photon Correlation Spectroscopy
+							body_vec[33]=1.0
+						if item[1]=='C78871':	#X-Ray Photoelectron Spectroscopy
+							body_vec[34]=1.0
+						if item[1]=='C142750':	#web server
+							body_vec[35]=1.0
+						if item[1]=='C47907':	#compiler
+							body_vec[36]=1.0
+						if item[1]=='C142380' or item[1]=='C80012':	#HTML
+							body_vec[37]=1.0
+						if item[1]=='C69302':	#R programming
+							body_vec[38]=1.0
+						if item[1]=='C75940' or item[1]=='C81018':	#Hidden Markov
+							body_vec[39]=1.0
+						if item[1]=='C142403' or item[1]=='C142404':	#Bayesian
+							body_vec[40]=1.0
 			except:
 				continue
-			with open(homedir+"/temp/tmp1.csv",'r',encoding='utf-8') as cf:
-				rd=csv.reader(cf)
-				for item in rd:
-					if item[0]=="Mention":
-						continue
-					if item[1]=='C38054':	#EEG
-						body_vec[0]=1.0
-					if item[1]=='C16809':	#MRI
-						body_vec[1]=1.0
-					if item[1]=='C116454':	#fMRI
-						body_vec[2]=1.0
-					if item[1]=='C116655':	#TMS
-						body_vec[3]=1.0
-					if item[1]=='C129862':	#tDCS
-						body_vec[4]=1.0
-					if item[1]=='C16811':	#MEG
-						body_vec[5]=1.0
-					if item[1]=='C78814' or item[1]=='C78815':	#SEM
-						body_vec[6]=1.0
-					if item[1]=='C78860' or item[1]=='C78813':	#TEM
-						body_vec[7]=1.0
-					if item[1]=='C17374':	#STM
-						body_vec[8]=1.0
-					if item[1]=='C78804':	#AFM
-						body_vec[9]=1.0
-					if item[1]=='C17753' or item[1]=='C122390' or item[1]=='C116477' or item[1]=='C116481':	#Confocal
-						body_vec[10]=1.0
-					if item[1]=='C93040':	#Alcohol
-						body_vec[11]=1.0
-					if item[1]=='C35386' or item[1]=='C35387' or item[1]=='C34445':	#Cannabis
-						body_vec[12]=1.0
-					if item[1]=='C34492' or item[1]=='C35389' or item[1]=='C35388':	#Cocaine
-						body_vec[13]=1.0
-					if item[1]=='C34694':	#Heroin
-						body_vec[14]=1.0
-					if item[1]=='C70989' or item[1]=='C54203' or item[1]=='C15985':	#Nicotine
-						body_vec[15]=1.0
-					if item[1]=='C73850':	#duck
-						body_vec[16]=1.0
-					if item[1]=='C91813':	#quail
-						body_vec[17]=1.0
-					if item[1]=='C91812':	#pigeon
-						body_vec[18]=1.0
-					if item[1]=='C77098':	#leghorn chicken
-						body_vec[19]=1.0
-					if item[1]=='C76362':	#broiler chicken
-						body_vec[20]=1.0
-					if item[1]=='C17611':	#Bronchoscope
-						body_vec[21]=1.0
-					if item[1]=='C17613':	#Colonoscope
-						body_vec[22]=1.0
-					if item[1]=='C17614':	#Colposcope
-						body_vec[23]=1.0
-					if item[1]=='C17616':	#Cystoscope
-						body_vec[24]=1.0
-					if item[1]=='C17620':	#Gastroscope
-						body_vec[25]=1.0
-					if item[1]=='C28167':	#Laparoscope
-						body_vec[26]=1.0
-					if item[1]=='C17618':	#Laryngoscope
-						body_vec[27]=1.0
-					if item[1]=='C85574':	#Atomic Absorption Spectroscopy
-						body_vec[28]=1.0
-					if item[1]=='C94374':	#Optical Spectroscopy
-						body_vec[29]=1.0
-					if item[1]=='C17157':	#Raman Spectroscopy
-						body_vec[30]=1.0
-					if item[1]=='C78869':	#Energy Dispersive Spectroscopy
-						body_vec[31]=1.0
-					if item[1]=='C16810':	#Magnetic Resonance Spectroscopy
-						body_vec[32]=1.0
-					if item[1]=='C62329':	#Photon Correlation Spectroscopy
-						body_vec[33]=1.0
-					if item[1]=='C78871':	#X-Ray Photoelectron Spectroscopy
-						body_vec[34]=1.0
-					if item[1]=='C142750':	#web server
-						body_vec[35]=1.0
-					if item[1]=='C47907':	#compiler
-						body_vec[36]=1.0
-					if item[1]=='C142380' or item[1]=='C80012':	#HTML
-						body_vec[37]=1.0
-					if item[1]=='C69302':	#R programming
-						body_vec[38]=1.0
-					if item[1]=='C75940' or item[1]=='C81018':	#Hidden Markov
-						body_vec[39]=1.0
-					if item[1]=='C142403' or item[1]=='C142404':	#Bayesian
-						body_vec[40]=1.0
 			sample_list.append(abs_vec+body_vec)
 			if len(sample_list)>=_batch:
 				N_all=np.array(sample_list)
