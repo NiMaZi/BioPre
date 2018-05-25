911a92f92893ad52e9d57eadfcf0b9b6246af1e7
NiMaZi <zhengyl940425@gmail.com>
2018-05-25 02:03:28 +0200

    try opt parser

--- elastic_search.py
+++
@@ -1,2 +0,0 @@
-from elasticsearch import Elasticsearch
-es = Elasticsearch()
--- train.py
+++ train.py
@@ -1,12 +1,74 @@
-import utils.util as util
+import csv
+import argparse
+import numpy as np
+from utils import util
 from models.bownn import BOWNN
 
+parser=argparse.ArgumentParser(description='train.py')
+parser.add_argument('-config',default=40,type=str,help="config file")
+opt=parser.parse_args()
+
+def train(model,folder,in_dict,out_dict,volume,batch_size=1024,epochs=5):
+	cc2vid_input=util.load_sups(in_dict)
+	cc2vid_output=util.load_sups(out_dict)
+	sample_list=[]
+
+	for i in range(0,volume):
+		abs_vec=[0.0 for i in range(0,len(cc2vid_input))]
+		abs_count=0.0
+
+		with open(folder+"/abs"+str(i)+".csv",'r',encoding='utf-8') as cf:
+			rd=csv.reader(cf)
+			for item in rd:
+				if item[0]=="Mention":
+					continue
+				try:
+					abs_vec[cc2vid[item[1]]]+=1.0
+					abs_count+=1.0
+				except:
+					pass
+		if not abs_count:
+			continue
+
+		abs_vec=list(np.array(abs_vec)/abs_count)
+
+		body_vec=[0.0 for i in range(0,len(cc2vid_output))]
+		body_count=0.0
+
+		with open(folder+"/body"+str(i)+".csv",'r',encoding='utf-8') as cf:
+			rd=csv.reader(cf)
+			for item in rd:
+				if item[0]=="Mention":
+					continue
+				try:
+					body_vec[cc2vid[item[1]]]+=1.0
+					body_count+=1.0
+				except:
+					pass
+		if not body_count:
+			continue
+
+		body_vec=list(np.array(body_vec)/body_count)
+		sample_list.append(abs_vec+body_vec)
+
+		if len(sample_list)>=batch_size:
+			N_all=np.array(sample_list)
+			X_train=N_all[:,:len(cc2vid_input)]
+			Y_train=np.clip(np.ceil(N_all[:,len(cc2vid_input):])-np.ceil(X_train),0.0,1.0)
+			model.model.fit(X_train,Y_train,batch_size=batch_size,verbose=0,epochs=epochs)
+			sample_list=[]
+	
+	if len(sample_list):
+		N_all=np.array(sample_list)
+		X_train=N_all[:,:len(cc2vid_input)]
+		Y_train=np.clip(np.ceil(N_all[:,len(cc2vid_input):])-np.ceil(X_train),0.0,1.0)
+		model.model.fit(X_train,Y_train,batch_size=batch_size,verbose=0,epochs=epochs)
 
 def main():
-	myModel=BOWNN(40)
-	d=myModel.get_d()
-	e=util.bucket()
-	print(d,e)
+	print(opt)
+	"""get arguments"""
+	"""intialize model"""
+	# train()
 
 if __name__=='__main__':
 	main()
